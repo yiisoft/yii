e2516e14c4f5fd013337d98dbfa9103422adecc8
qiang.xue <qiang.xue@gmail.com>
2008-10-09 16:08:38 +0000

    Added CWebServiceAction.classMap. Added CFlexWidget.
    git-svn-id: http://yii.googlecode.com/svn/trunk@57 c7f931c7-7552-0410-b027-2fb3d89d9100

---
+++ framework/web/widgets/CFlexWidget.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * CFlexWidget class file.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright &copy; 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+/**
+ * CFlexWidget embeds a Flex application into a page.
+ *
+ * To use CFlexWidget, set {@link name} to be the Flex application name
+ * (without the .swf suffix), and set {@link baseUrl} to be URL (without the ending slash)
+ * of the directory containing the SWF file of the Flex application.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @version $Id$
+ * @package system.web.widgets
+ * @since 1.0
+ */
+class CFlexWidget extends CWidget
+{
+	/**
+	 * @var string name of the Flex application.
+	 * This should be the SWF file name without the ".swf" suffix.
+	 */
+	public $name;
+	/**
+	 * @var string the base URL of the Flex application.
+	 * This refers to the URL of the directory containing the SWF file.
+	 */
+	public $baseUrl;
+	/**
+	 * @var string width of the application region. Defaults to 450.
+	 */
+	public $width=450;
+	/**
+	 * @var string height of the application region. Defaults to 300.
+	 */
+	public $height=300;
+	/**
+	 * @var string quality of the animation. Defaults to 'high'.
+	 */
+	public $quality='high';
+	/**
+	 * @var string background color of the application region. Defaults to '#FFFFFF', meaning white.
+	 */
+	public $bgColor='#FFFFFF';
+	/**
+	 * @var string align of the application region. Defaults to 'middle'.
+	 */
+	public $align='middle';
+	/**
+	 * @var string the access method of the script. Defaults to 'sameDomain'.
+	 */
+	public $allowScriptAccess='sameDomain';
+	/**
+	 * @var string the HTML content to be displayed if Flash player is not installed.
+	 */
+	public $altHtmlContent;
+	/**
+	 * @var boolean whether history should be enabled. Defaults to true.
+	 */
+	public $enableHistory=true;
+	/**
+	 * @var array parameters to be passed to the Flex application.
+	 */
+	public $flashVars=array();
+
+	/**
+	 * Renders the widget.
+	 */
+	public function run()
+	{
+		if(empty($this->name))
+			throw new CException(Yii::t('yii#CFlexWidget.name cannot be empty.'));
+		if(empty($this->baseUrl))
+			throw new CException(Yii::t('yii#CFlexWidget.baseUrl cannot be empty.'));
+		if($this->altHtmlContent===null)
+			$this->altHtmlContent=Yii::t('yii#This content requires the <a href="http://www.adobe.com/go/getflash/">Adobe Flash Player</a>.');
+
+		$cs=$this->getController()->getClientScript();
+		$cs->registerScriptFile($this->baseUrl.'/AC_OETags.js');
+
+		if($this->enableHistory)
+			$cs->registerScriptFile($this->baseUrl.'/history.js');
+
+		$this->render('flexWidget',array('flashVars'=>$this->generateFlashVars()));
+	}
+
+	/**
+	 * Generates the properly quoted flash parameter string.
+	 * @return string the flash parameter string.
+	 */
+	protected function generateFlashVars()
+	{
+		$params=array();
+		foreach($this->flashVars as $k=>$v)
+			$params[]=urlencode($k).'='.urlencode($v);
+		$flashVars=implode('&',$params);
+
+		if($this->enableHistory)
+		{
+			$historyUrl=urlencode($this->baseUrl.'/history.htm');
+			if($flashVars!=='')
+				$flashVars.='&';
+			$flashVars.="historyUrl={$historyUrl}&lconid=";
+			return "'".CJavaScript::quote($flashVars)."' + lc_id";
+		}
+		else
+			return "'".CJavaScript::quote($flashVars)."'";
+	}
+}
