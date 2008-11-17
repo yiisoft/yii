63ed017273e62b8c91cfe3ec6759daf1f75c00b1
qiang.xue <qiang.xue@gmail.com>
2008-11-17 18:44:50 +0000

    Fixed a bug in CFilterValidator about setting filter property.
    git-svn-id: http://yii.googlecode.com/svn/trunk@234 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/validators/CFilterValidator.php
+++ framework/validators/CFilterValidator.php
@@ -42,7 +42,7 @@ class CFilterValidator extends CValidator
 	 */
 	protected function validateAttribute($object,$attribute)
 	{
-		if($this->filter===null || is_callable($this->filter))
+		if($this->filter===null || !is_callable($this->filter))
 			throw new CException(Yii::t('yii','The "filter" property must be specified with a valid callback.'));
 		$object->$attribute=call_user_func_array($this->filter,$object->$attribute);
 	}
