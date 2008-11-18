a0db5bd9aaf085df5abd8ef4fa12b27658101b77
qiang.xue <qiang.xue@gmail.com>
2008-11-18 19:26:27 +0000

    refactored CLinkPager.
    git-svn-id: http://yii.googlecode.com/svn/trunk@241 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/web/widgets/pagers/CLinkPager.php
+++ framework/web/widgets/pagers/CLinkPager.php
@@ -18,45 +18,40 @@
  */
 class CLinkPager extends CBasePager
 {
+	const CSS_FIRST_PAGE='first';
+	const CSS_LAST_PAGE='last';
+	const CSS_PREVIOUS_PAGE='previous';
+	const CSS_NEXT_PAGE='previous';
+	const CSS_INTERNAL_PAGE='page';
+	const CSS_HIDDEN_PAGE='hidden';
+	const CSS_SELECTED_PAGE='selected';
+
 	/**
 	 * @var integer maximum number of page buttons that can be displayed. Defaults to 10.
 	 */
 	public $maxButtonCount=10;
 	/**
-	 * @var string the text label for the next page button. Defaults to 'Next >>'.
+	 * @var string the text label for the next page button. Defaults to 'Next &gt;'.
 	 * Note that the label must be HTML encoded.
 	 */
-	public $nextPageLabel='Next &gt;&gt;';
+	public $nextPageLabel='Next &gt;';
 	/**
-	 * @var string the text label for the previous page button. Defaults to '<< Prev'.
+	 * @var string the text label for the previous page button. Defaults to '&lt; Previous'.
 	 * Note that the label must be HTML encoded.
 	 */
-	public $prevPageLabel='&lt;&lt; Prev';
+	public $prevPageLabel='&lt; Previous';
 	/**
-	 * @var string the text label for the first page button. Defaults to '|<< First'.
+	 * @var string the text label for the first page button. Defaults to '&lt;&lt; First'.
 	 * Note that the label must be HTML encoded.
 	 */
-	public $firstPageLabel='|&lt;&lt; First';
+	public $firstPageLabel='&lt;&lt; First';
 	/**
-	 * @var string the text label for the last page button. Defaults to 'Last >>|'.
+	 * @var string the text label for the last page button. Defaults to 'Last &gt;&gt;'.
 	 * Note that the label must be HTML encoded.
 	 */
-	public $lastPageLabel='Last &gt;&gt;|';
+	public $lastPageLabel='Last &gt;&gt;';
 	/**
-	 * @var boolean whether to show the first page button. Defaults to false.
-	 * Note that the label must be HTML encoded.
-	 */
-	public $showFirstPageButton=false;
-	/**
-	 * @var boolean whether to show the last page button. Defaults to false.
-	 */
-	public $showLastPageButton=false;
-	/**
-	 * @var string separator between page buttons. Defaults to "\n".
-	 */
-	public $buttonSeparator="\n";
-	/**
-	 * @var string the text shown before page buttons.
+	 * @var string the text shown before page buttons. Defaults to 'Go to page: '.
 	 */
 	public $header='Go to page: ';
 	/**
@@ -64,7 +59,14 @@ class CLinkPager extends CBasePager
 	 */
 	public $footer='';
 	/**
-	 * @var array HTML attributes for the enclosing 'div' tag.
+	 * @var mixed the CSS file used for the widget. Defaults to null, meaning
+	 * using the default CSS file included together with the widget.
+	 * If false, no CSS file will be used. Otherwise, the specified CSS file
+	 * will be included when using this widget.
+	 */
+	public $cssFile;
+	/**
+	 * @var array HTML attributes for the pager container tag.
 	 */
 	public $htmlOptions=array();
 
@@ -75,38 +77,74 @@ class CLinkPager extends CBasePager
 	 */
 	public function run()
 	{
-		if(($pageCount=$this->getPageCount())<=1)
+		$buttons=$this->createPageButtons();
+
+		if(empty($buttons))
 			return;
 
-		list($beginPage,$endPage)=$this->getPageRange();
-		$currentPage=$this->getCurrentPage(false); // currentPage is calculated in getPageRange()
-		$controller=$this->getController();
-		$params=$_GET;
-		$buttons=array();
+		$this->registerClientScript();
 
-		// first page
-		if($this->showFirstPageButton)
-			$buttons[]=$this->createPageButton($this->firstPageLabel,0,$currentPage,'first',$beginPage>0);
-
-		// prev page
-		$buttons[]=$this->createPageButton($this->prevPageLabel,$currentPage-1,$currentPage,'prev',$currentPage>0);
-
-		// internal pages
-		for($i=$beginPage;$i<=$endPage;++$i)
-			$buttons[]=$this->createPageButton($i+1,$i,$currentPage);
-
-		// next page
-		$buttons[]=$this->createPageButton($this->nextPageLabel,$currentPage+1,$currentPage,'next',$currentPage<$pageCount-1);
-
-		// last page
-		if($this->showLastPageButton)
-			$buttons[]=$this->createPageButton($this->lastPageLabel,$pageCount-1,$currentPage,'last',$endPage<$pageCount-1);
-
-		$content=implode($this->buttonSeparator,$buttons);
 		$htmlOptions=$this->htmlOptions;
 		if(!isset($htmlOptions['id']))
 			$htmlOptions['id']=$this->getId();
-		echo CHtml::tag('div',$htmlOptions,$this->header.$content.$this->footer);
+		if(!isset($htmlOptions['class']))
+			$htmlOptions['class']='yiiPager';
+		echo $this->header;
+		echo CHtml::tag('ul',$htmlOptions,implode("\n",$buttons));
+		echo $this->footer;
+	}
+
+	/**
+	 * Creates the page buttons.
+	 * @return array a list of page buttons (in HTML code).
+	 */
+	protected function createPageButtons()
+	{
+		if(($pageCount=$this->getPageCount())<=1)
+			return array();
+
+		list($beginPage,$endPage)=$this->getPageRange();
+		$currentPage=$this->getCurrentPage(false); // currentPage is calculated in getPageRange()
+		$buttons=array();
+
+		// first page
+		$buttons[]=$this->createPageButton($this->firstPageLabel,0,self::CSS_FIRST_PAGE,$beginPage<=0,false);
+
+		// prev page
+		if(($page=$currentPage-1)<0)
+			$page=0;
+		$buttons[]=$this->createPageButton($this->prevPageLabel,$page,self::CSS_PREVIOUS_PAGE,$currentPage<=0,false);
+
+		// internal pages
+		for($i=$beginPage;$i<=$endPage;++$i)
+			$buttons[]=$this->createPageButton($i+1,$i,self::CSS_INTERNAL_PAGE,false,$i==$currentPage);
+
+		// next page
+		if(($page=$currentPage+1)>=$pageCount-1)
+			$page=$pageCount-1;
+		$buttons[]=$this->createPageButton($this->nextPageLabel,$page,self::CSS_NEXT_PAGE,$currentPage>=$pageCount-1,false);
+
+		// last page
+		$buttons[]=$this->createPageButton($this->lastPageLabel,$pageCount-1,self::CSS_LAST_PAGE,$endPage>=$pageCount-1,false);
+
+		return $buttons;
+	}
+
+	/**
+	 * Creates a page button.
+	 * You may override this method to customize the page buttons.
+	 * @param string the text label for the button
+	 * @param integer the page number
+	 * @param string the CSS class for the page button. This could be 'page', 'first', 'last', 'next' or 'previous'.
+	 * @param boolean whether this page button is visible
+	 * @param boolean whether this page button is selected
+	 * @return string the generated button
+	 */
+	protected function createPageButton($label,$page,$class,$hidden,$selected)
+	{
+		if($hidden || $selected)
+			$class.=' '.($hidden ? self::CSS_HIDDEN_PAGE : self::CSS_SELECTED_PAGE);
+		return '<li class="'.$class.'">'.CHtml::link($label,$this->createPageUrl($page)).'</li>';
 	}
 
 	/**
@@ -130,28 +168,14 @@ class CLinkPager extends CBasePager
 	}
 
 	/**
-	 * Creates a page button.
-	 * You may override this method to customize the page buttons.
-	 * Hint: you can use the label parameter to determine which type of button
-	 * is being created. For example, if the label is the same as {@link firstPageLabel},
-	 * then it is creating the first page button.
-	 * @param string the text label for the button
-	 * @param integer the page number
-	 * @param integer the current page number
-	 * @param string the CSS class for the page button. This could be 'page', 'first', 'last', 'next' or 'prev'.
-	 * @param boolean whether this page button is visible
-	 * @return string the generated button
+	 * Registers the needed client scripts (mainly CSS file).
 	 */
-	protected function createPageButton($label,$page,$currentPage,$cssClass='page',$visible=true)
+	protected function registerClientScript()
 	{
-		$options=array();
-		if($cssClass!=='')
-			$options['class']=$cssClass;
-		if(!$visible)
-			$options['style']='display:none';
-		if($page===$currentPage)
-			return CHtml::tag('span',$options,$label);
-		else
-			return CHtml::tag('span',$options,CHtml::link($label,$this->createPageUrl($page)));
+		$cs=$this->getController()->getClientScript();
+		if($this->cssFile!==null)
+			$cs->registerCssFile($this->cssFile);
+		else if($this->cssFile!==false)
+			$cs->registerCssFile(CHtml::asset(Yii::getPathOfAlias('system.web.widgets.pagers.pager').'.css'));
 	}
 }
