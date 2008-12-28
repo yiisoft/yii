1a0040c3548c2b177432a6009cc1c1c867be5c33
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2008-12-28 16:12:25 +0000

    minor change to yiic webapp.

--- framework/cli/views/webapp/protected/views/site/index.php
+++ framework/cli/views/webapp/protected/views/site/index.php
@@ -18,19 +18,19 @@ This is the homepage of <em><?php echo Yii::app()->name; ?></em>. You may modify
 
 <h3>What's Next</h3>
 <ul>
-	<li>Implement new actions in <tt>SiteController</tt>, and create corresponding views	under <?php echo Yii::app()->viewPath . DIRECTORY_SEPARATOR . 'site'; ?></li>
+	<li>Implement new actions in <tt>SiteController</tt>, and create corresponding views under <?php echo Yii::app()->viewPath . DIRECTORY_SEPARATOR . 'site'; ?></li>
 	<li>Create new controllers and actions manually or using the <tt>yiic</tt> tool.
 	<li>If your Web application should be driven by database, do the following:
 		<ul>
-			<li>Set up database connection by configuring the <code>db</code> component in the application configuration
+			<li>Set up a database connection by configuring the <code>db</code> component in the application configuration
 			<tt><?php echo Yii::app()->basePath . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR .'main.php'; ?></tt></li>
 			<li>Create model classes under the directory
 			<tt><?php echo Yii::app()->basePath . DIRECTORY_SEPARATOR . 'models'; ?></tt></li>
-			<li>Implement CRUD operations for a model class. For example, for the <tt>Post</tt> model class,
+			<li>Implement CRUD operations for each model. For example, for the <tt>Post</tt> model class,
 			you would create a <tt>PostController</tt> class together with <tt>create</tt>, <tt>read</tt>,
 			<tt>update</tt> and <tt>delete</tt> actions.</li>
 		</ul>
-		Note, the <tt>yiic</tt> tool can automate the task of creating model classes and CRUD operations.
+		Note that the <tt>yiic</tt> tool can automate the task of creating model classes and CRUD operations.
 	</li>
 </ul>
 
