b0ae4384a1d02f614fc55b23a200862a9122701d
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-01-26 22:06:49 +0000

    Added new translations.

--- requirements/messages/nl/yii.php
+++ requirements/messages/nl/yii.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * Message translations.
  *
@@ -16,28 +16,28 @@
  * @version $Id: $
  */
 return array (
-  '$_SERVER does not have {vars}.' => '',
-  '$_SERVER variable' => '',
-  '$_SERVER["SCRIPT_FILENAME"] must be the same as the entry script file path.' => '',
-  'APC extension' => '',
-  'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>' => '',
-  'DOM extension' => '',
-  'Either $_SERVER["REQUEST_URI"] or $_SERVER["QUERY_STRING"] must exist.' => '',
-  'GD extension' => '',
-  'Mcrypt extension' => '',
-  'Memcache extension' => '',
-  'PCRE extension' => '',
-  'PDO MySQL extension' => '',
-  'PDO PostgreSQL extension' => '',
-  'PDO SQLite extension' => '',
-  'PDO extension' => '',
-  'PHP 5.1.0 or higher is required.' => '',
-  'PHP version' => '',
-  'Reflection extension' => '',
-  'SOAP extension' => '',
-  'SPL extension' => '',
-  'This is required if you are using MySQL database.' => '',
-  'This is required if you are using PostgreSQL database.' => '',
-  'This is required if you are using SQLite database.' => '',
-  'Unable to determine URL path info. Please make sure $_SERVER["PATH_INFO"] (or $_SERVER["PHP_SELF"] and $_SERVER["SCRIPT_NAME"]) contains proper value.' => '',
+  '$_SERVER does not have {vars}.' => '$_SERVER heeft geen {vars}.',
+  '$_SERVER variable' => '$_SERVER variabele',
+  '$_SERVER["SCRIPT_FILENAME"] must be the same as the entry script file path.' => '$_SERVER["SCRIPT_FILENAME"] moet hetzelfde zijn als het bestandspad van het startscript.',
+  'APC extension' => 'APC extensie',
+  'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>' => 'Alle <a href="http://www.yiiframework.com/doc/api/#system.db">DB-gerelateerde classes</a>',
+  'DOM extension' => 'DOM extensie',
+  'Either $_SERVER["REQUEST_URI"] or $_SERVER["QUERY_STRING"] must exist.' => 'Of $_SERVER["REQUEST_URI"] of $_SERVER["QUERY_STRING"] moet gedefinieerd zijn.',
+  'GD extension' => 'GD extensie',
+  'Mcrypt extension' => 'Mcrypt extensie',
+  'Memcache extension' => 'Memcache extensie',
+  'PCRE extension' => 'PCRE extensie',
+  'PDO MySQL extension' => 'PDO MySQL extensie',
+  'PDO PostgreSQL extension' => 'PDO PostgreSQL extensie',
+  'PDO SQLite extension' => 'PDO SQLite extensie',
+  'PDO extension' => 'PDO extensie',
+  'PHP 5.1.0 or higher is required.' => 'PHP 5.1.0 of hoger is vereist.',
+  'PHP version' => 'PHP versie',
+  'Reflection extension' => 'Reflection extensie',
+  'SOAP extension' => 'SOAP extensie',
+  'SPL extension' => 'SPL extensie',
+  'This is required if you are using MySQL database.' => 'Dit is vereist als een MySQL database wordt gebruikt.',
+  'This is required if you are using PostgreSQL database.' => 'Dit is vereist als een PostgreSQL database wordt gebruikt.',
+  'This is required if you are using SQLite database.' => 'Dit is vereist als een SQLite database wordt gebruikt.',
+  'Unable to determine URL path info. Please make sure $_SERVER["PATH_INFO"] (or $_SERVER["PHP_SELF"] and $_SERVER["SCRIPT_NAME"]) contains proper value.' => 'Niet in staat om informatie over het URL pad te verkrijgen. Zorg er a.u.b. voor dat $_SERVER["PATH_INFO"] (of $_SERVER["PHP_SELF"] en $_SERVER["SCRIPT_NAME"]) een juiste waarde bevatten.',
 );
---
+++ requirements/views/zh_tw/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta http-equiv="content-language" content="en"/>
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<title>Yii系統需求檢查</title>
+</head>
+
+<body>
+<div id="page">
+
+<div id="header">
+<h1>Yii系統需求檢查</h1>
+</div><!-- header-->
+
+<div id="content">
+<h2>檢查內容說明</h2>
+<p>
+本網頁用於確認您的伺服器組態設定是否能滿足執行<a href="http://www.yiiframework.com/">Yii</a> Web應用程式的要求. 它將檢查伺服器是否使用正確的PHP版本, 是否安裝了合適的PHP extension, 以及確認php.ini檔案是否正確設定.
+</p>
+
+<h2>檢查結果</h2>
+<p>
+<?php if($result>0): ?>
+恭喜! 您的伺服器組態設定完全符合Yii的要求.
+<?php elseif($result<0): ?>
+您的伺服器組態設定符合Yii的最低要求. 請注意下列警告(如果您的應用程式會需要使用到相關功能).
+<?php else: ?>
+您的伺服器組態設定未能滿足Yii的要求.
+<?php endif; ?>
+</p>
+
+<h2>詳細結果</h2>
+
+<table class="result">
+<tr><th>項目名稱</th><th>結果</th><th>使用者</th><th>備註</th></tr>
+<?php foreach($requirements as $requirement): ?>
+<tr>
+	<td>
+	<?php echo $requirement[0]; ?>
+	</td>
+	<td class="<?php echo $requirement[2] ? 'passed' : ($requirement[1] ? 'failed' : 'warning'); ?>">
+	<?php echo $requirement[2] ? '通過' : '未通過'; ?>
+	</td>
+	<td>
+	<?php echo $requirement[3]; ?>
+	</td>
+	<td>
+	<?php echo $requirement[4]; ?>
+	</td>
+</tr>
+<?php endforeach; ?>
+</table>
+
+<table>
+<tr>
+<td class="passed">&nbsp;</td><td>通過</td>
+<td class="failed">&nbsp;</td><td>未通過</td>
+<td class="warning">&nbsp;</td><td>警告</td>
+</tr>
+</table>
+
+</div><!-- content -->
+
+<div id="footer">
+<?php echo $serverInfo; ?>
+</div><!-- footer -->
+
+</div><!-- page -->
+</body>
+</html>
