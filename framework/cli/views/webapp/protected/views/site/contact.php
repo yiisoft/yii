52ef9909d044581bad3dae595715e37564511220
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-01-30 18:40:40 +0000

    Enhanced label display for automatically generated code.

--- framework/cli/views/webapp/protected/views/site/contact.php
+++ framework/cli/views/webapp/protected/views/site/contact.php
@@ -13,30 +13,35 @@ If you have business inquries or other questions, please fill out the following 
 </p>
 
 <div class="yiiForm">
+
+<p>
+Fields with <span class="required">*</span> are required.
+</p>
+
 <?php echo CHtml::form(); ?>
 
 <?php echo CHtml::errorSummary($contact); ?>
 
 <div class="simple">
-<?php echo CHtml::activeLabel($contact,'name'); ?>
+<?php echo CHtml::activeLabelEx($contact,'name'); ?>
 <?php echo CHtml::activeTextField($contact,'name'); ?>
 </div>
 <div class="simple">
-<?php echo CHtml::activeLabel($contact,'email'); ?>
+<?php echo CHtml::activeLabelEx($contact,'email'); ?>
 <?php echo CHtml::activeTextField($contact,'email'); ?>
 </div>
 <div class="simple">
-<?php echo CHtml::activeLabel($contact,'subject'); ?>
+<?php echo CHtml::activeLabelEx($contact,'subject'); ?>
 <?php echo CHtml::activeTextField($contact,'subject',array('size'=>60,'maxlength'=>128)); ?>
 </div>
 <div class="simple">
-<?php echo CHtml::activeLabel($contact,'body'); ?>
+<?php echo CHtml::activeLabelEx($contact,'body'); ?>
 <?php echo CHtml::activeTextArea($contact,'body',array('rows'=>6, 'cols'=>50)); ?>
 </div>
 
 <?php if(extension_loaded('gd')): ?>
 <div class="simple">
-	<?php echo CHtml::activeLabel($contact,'verifyCode'); ?>
+	<?php echo CHtml::activeLabelEx($contact,'verifyCode'); ?>
 	<div>
 	<?php $this->widget('CCaptcha'); ?>
 	<?php echo CHtml::activeTextField($contact,'verifyCode'); ?>
--- framework/cli/views/webapp/protected/views/site/login.php
+++ framework/cli/views/webapp/protected/views/site/login.php
@@ -3,17 +3,20 @@
 <h1>Login</h1>
 
 <div class="yiiForm">
+<p>
+Fields with <span class="required">*</span> are required.
+</p>
 <?php echo CHtml::form(); ?>
 
 <?php echo CHtml::errorSummary($form); ?>
 
 <div class="simple">
-<?php echo CHtml::activeLabel($form,'username'); ?>
+<?php echo CHtml::activeLabelEx($form,'username'); ?>
 <?php echo CHtml::activeTextField($form,'username') ?>
 </div>
 
 <div class="simple">
-<?php echo CHtml::activeLabel($form,'password'); ?>
+<?php echo CHtml::activeLabelEx($form,'password'); ?>
 <?php echo CHtml::activePasswordField($form,'password') ?>
 <p class="hint">
 Hint: You may login with <tt>demo/demo</tt> or <tt>admin/admin</tt>.
