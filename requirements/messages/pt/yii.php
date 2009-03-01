d568cf2a5680c24530fc3e0b49a7634392f552e4
qiang.xue <qiang.xue@gmail.com>
2009-03-01 01:32:35 +0000

    merge from 1.0
    git-svn-id: http://yii.googlecode.com/svn/trunk@777 c7f931c7-7552-0410-b027-2fb3d89d9100

--- requirements/messages/pt/yii.php
+++ requirements/messages/pt/yii.php
@@ -16,11 +16,11 @@
  * @version $Id: $
  */
 return array (
-  '$_SERVER does not have {vars}.' => '$_SERVER não possui {vars}',
+  '$_SERVER does not have {vars}.' => '$_SERVER não possui {vars}.',
   '$_SERVER variable' => 'variável $_SERVER',
-  '$_SERVER["SCRIPT_FILENAME"] must be the same as the entry script file path.' => '$_SERVER["SCRIPT_FILENAME"] precisa ser o mesmo que o arquivo de script de entrada',
+  '$_SERVER["SCRIPT_FILENAME"] must be the same as the entry script file path.' => '$_SERVER["SCRIPT_FILENAME"] precisa ser o mesmo que o arquivo de script de entrada.',
   'APC extension' => 'extensão APC',
-  'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>' => 'Todas as classes relacionadas a <a href="http://www.yiiframework.com/doc/api/#system.db">banco de dados</a>',
+  'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>' => 'Todas as classes relacionadas ao <a href="http://www.yiiframework.com/doc/api/#system.db">banco de dados</a>',
   'DOM extension' => 'extensão DOM',
   'Either $_SERVER["REQUEST_URI"] or $_SERVER["QUERY_STRING"] must exist.' => '$_SERVER["REQUEST_URI"] ou $_SERVER["QUERY_STRING"] é obrigatório.',
   'GD extension' => 'extensão GD',
@@ -31,13 +31,13 @@ return array (
   'PDO PostgreSQL extension' => 'extensão PDO_PostgreSQL',
   'PDO SQLite extension' => 'extensão PDO_SQLite',
   'PDO extension' => 'extensão PDO',
-  'PHP 5.1.0 or higher is required.' => 'PHP 5.1.0 ou mais recente é necessário',
+  'PHP 5.1.0 or higher is required.' => 'PHP 5.1.0 ou mais recente é necessário.',
   'PHP version' => 'versão PHP',
   'Reflection extension' => 'extensão Reflection',
   'SOAP extension' => 'extensão SOAP',
   'SPL extension' => 'extensão SPL',
-  'This is required if you are using MySQL database.' => 'Este item é obrigatório se você estiver utilizando MySQL',
-  'This is required if you are using PostgreSQL database.' => 'Este item é obrigatório se você estiver utilizando PostgreSQL',
-  'This is required if you are using SQLite database.' => 'Este item é obrigatório se você estiver utilizando SQLITE',
+  'This is required if you are using MySQL database.' => 'Este item é obrigatório se você estiver utilizando MySQL.',
+  'This is required if you are using PostgreSQL database.' => 'Este item é obrigatório se você estiver utilizando PostgreSQL.',
+  'This is required if you are using SQLite database.' => 'Este item é obrigatório se você estiver utilizando SQLite.',
   'Unable to determine URL path info. Please make sure $_SERVER["PATH_INFO"] (or $_SERVER["PHP_SELF"] and $_SERVER["SCRIPT_NAME"]) contains proper value.' => 'Não foi possível determinar o caminho da URL. Verifique se $_SERVER["PATH_INFO"] (ou $_SERVER["PHP_SELF"] e $_SERVER["SCRIPT_NAME"]) contem os valores corretos.',
 );
