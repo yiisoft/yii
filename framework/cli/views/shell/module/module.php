b88f411c5e76eb72e10da2a90f81cd4eb248581a
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-03-23 11:49:27 +0000

    fixed yiic module command about module init.

--- framework/cli/views/shell/module/module.php
+++ framework/cli/views/shell/module/module.php
@@ -2,10 +2,21 @@
 
 class <?php echo $moduleClass; ?> extends CWebModule
 {
-	public function init($config)
+	public function init()
 	{
-		parent::init($config);
 		// this method is called when the module is being created
 		// you may place code here to customize the module or the application
+	}
+
+	public function beforeControllerAction($controller, $action)
+	{
+		if(parent::beforeControllerAction($controller, $action))
+		{
+			// this method is called before any module controller action is performed
+			// you may place customized code here
+			return true;
+		}
+		else
+			return false;
 	}
 }
