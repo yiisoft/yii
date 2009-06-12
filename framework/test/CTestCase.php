ed259c3961f68e62cd6442666c4bf1800072aed8
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-06-12 03:29:03 +0000

    Finished all test framework classes.

--- framework/test/CTestCase.php
+++ framework/test/CTestCase.php
@@ -1,7 +1,23 @@
 <?php
+/**
+ * This file contains the CTestCase class.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright &copy; 2008-2009 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
 
 require_once('PHPUnit/Framework/TestCase.php');
 
+/**
+ * CTestCase is the base class for all test case classes.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @version $Id$
+ * @package system.test
+ * @since 1.1
+ */
 class CTestCase extends PHPUnit_Framework_TestCase
 {
 }
