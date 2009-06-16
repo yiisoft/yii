4d78d9eda61b722512b32c007dce7ef8e04fcac1
qiang.xue <qiang.xue@gmail.com>
2009-06-16 21:08:31 +0000

    Added test scripts for the blog demo.
    git-svn-id: http://yii.googlecode.com/svn/trunk@1132 c7f931c7-7552-0410-b027-2fb3d89d9100

---
+++ demos/blog/protected/tests/fixtures/Tag.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	array(
+		'name'=>'yii',
+	),
+	array(
+		'name'=>'blog',
+	),
+);
---
+++ demos/blog/protected/tests/fixtures/User.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	array(
+		'username'=>'demo',
+		'password'=>'fe01ce2a7fbac8fafaed7c982a04e229',
+		'email'=>'webmaster@example.com',
+	),
+);
