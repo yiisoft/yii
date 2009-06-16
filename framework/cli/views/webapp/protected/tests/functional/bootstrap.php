604015107a202555951ba61db3ef4804131d36b1
qiang.xue <qiang.xue@gmail.com>
2009-06-16 21:00:33 +0000

    refactored testing framework.
    git-svn-id: http://yii.googlecode.com/svn/trunk@1131 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/tests/functional/bootstrap.php
+++ framework/cli/views/webapp/protected/tests/functional/bootstrap.php
@@ -1,21 +1,9 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../../framework/yii.php';
+$yiit=dirname(__FILE__).'/../../../../framework/yiit.php';
 $config=dirname(__FILE__).'/../../config/test.php';
 
-// disable Yii error handling logic
-defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER',false);
-defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER',false);
-
-// the following server variables are defined to simulate Web-based execution
-$_SERVER['SCRIPT_NAME']='/'.basename(__FILE__);
-$_SERVER['SCRIPT_FILENAME']=__FILE__;
-
-require_once($yii);
-
-Yii::import('system.test.CTestCase');
-Yii::import('system.test.CDbTestCase');
-Yii::import('system.test.CWebTestCase');
+require_once($yiit);
 
 Yii::createWebApplication($config);
--- tests/unit/readme.txt
+++ tests/unit/readme.txt
@@ -4,7 +4,7 @@ PHPUnit 3.3+ and xdebug (for code coverage report) are required.
 To run a single unit test, use:
 >> phpunit path/to/test.php
 
-To run all tests, use:
->> phpunit --verbose
+To run all tests under a directory, use:
+>> phpunit --verbose path/to/directory
 
 Please use 'phpunit --help' to see more command line options.
