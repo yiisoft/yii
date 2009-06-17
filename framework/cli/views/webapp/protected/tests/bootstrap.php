a7425f1e8cacb33399f036d04d9e00b7514e92f8
qiang.xue <qiang.xue@gmail.com>
2009-06-17 17:53:53 +0000

    git-svn-id: http://yii.googlecode.com/svn/trunk@1140 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/tests/bootstrap.php
+++ framework/cli/views/webapp/protected/tests/bootstrap.php
@@ -5,5 +5,6 @@ $yiit=dirname(__FILE__).'/../../../framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 require_once($yiit);
+require_once(dirname(__FILE__).'/WebTestCase.php');
 
 Yii::createWebApplication($config);
