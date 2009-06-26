7540623f512f69b31cb74d1299336e3a869c8dd1
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-06-26 20:03:21 +0000

    Added 'csrf' to CHtml client options so that js-based form submission can submit CSRF token

--- framework/web/helpers/CHtml.php
+++ framework/web/helpers/CHtml.php
@@ -1614,6 +1614,9 @@ EOD;
 	 * <ul>
 	 * <li>submit: string, specifies the URL that the button should submit to. If empty, the current requested URL will be used.</li>
 	 * <li>params: array, name-value pairs that should be submitted together with the form. This is only used when 'submit' option is specified.</li>
+	 * <li>csrf: boolean, whether a CSRF token should be submitted when {@link CHttpRequest::enableCsrfValidation} is true. Defaults to false.
+	 * This option has been available since version 1.0.7. You may want to set this to be true if there is no enclosing
+	 * form around this element. This option is meaningful only when 'submit' option is set.</li>
 	 * <li>return: boolean, the return value of the javascript. Defaults to false, meaning that the execution of
 	 * javascript would not cause the default behavior of the event. This option has been available since version 1.0.2.</li>
 	 * <li>confirm: string, specifies the message that should show in a pop-up confirmation dialog.</li>
@@ -1646,14 +1649,16 @@ EOD;
 		$cs=Yii::app()->getClientScript();
 		$cs->registerCoreScript('jquery');
 
-		if(isset($htmlOptions['params']))
-			$params=CJavaScript::encode($htmlOptions['params']);
-		else
-			$params='{}';
-
 		if(isset($htmlOptions['submit']))
 		{
 			$cs->registerCoreScript('yii');
+			$request=Yii::app()->getRequest();
+			if($request->enableCsrfValidation && isset($htmlOptions['csrf']) && $htmlOptions['csrf'])
+				$htmlOptions['params'][$request->csrfTokenName]=$request->getCsrfToken();
+			if(isset($htmlOptions['params']))
+				$params=CJavaScript::encode($htmlOptions['params']);
+			else
+				$params='{}';
 			if($htmlOptions['submit']!=='')
 				$url=CJavaScript::quote(self::normalizeUrl($htmlOptions['submit']));
 			else
@@ -1674,7 +1679,7 @@ EOD;
 		}
 
 		$cs->registerScript('Yii.CHtml.#'.$id,"jQuery('#$id').$event(function(){{$handler}});");
-		unset($htmlOptions['params'],$htmlOptions['submit'],$htmlOptions['ajax'],$htmlOptions['confirm'],$htmlOptions['return']);
+		unset($htmlOptions['params'],$htmlOptions['submit'],$htmlOptions['ajax'],$htmlOptions['confirm'],$htmlOptions['return'],$htmlOptions['csrf']);
 	}
 
 	/**
