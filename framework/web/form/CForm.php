c677a8910b110910963cc633065c79bb2b9fc7b1
qiang.xue <qiang.xue@gmail.com>
2009-07-10 13:37:06 +0000

    git-svn-id: http://yii.googlecode.com/svn/trunk@1234 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/web/form/CForm.php
+++ framework/web/form/CForm.php
@@ -445,11 +445,19 @@ class CForm extends CFormElement implements ArrayAccess
 
 	/**
 	 * Renders a single element which could be an input element, a sub-form, a string, or a button.
-	 * @param CFormElement the form element to be rendered
+	 * @param mixed the form element to be rendered. This can be either a {@link CFormElement} instance
+	 * or a string representing the name of the form element.
 	 * @return string the rendering result
 	 */
 	public function renderElement($element)
 	{
+		if(is_string($element))
+		{
+			if(($e=$this[$element])===null && ($e=$this->getButtons()->itemAt($element))===null)
+				return $element;
+			else
+				$element=$e;
+		}
 		if($element->getVisible())
 		{
 			if($element instanceof CFormInputElement)
