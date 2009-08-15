bcbb63473b36ba81ff0b4f69f9cf3f938188e6c0
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-08-15 19:58:08 +0000

    (Fixes issue 505)

--- framework/views/profile-summary.php
+++ framework/views/profile-summary.php
@@ -1,5 +1,5 @@
 <!-- start profiling summary -->
-<table width="100%" cellpadding="2" style="border-spacing:1px;font:11px Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;color:#666666;">
+<table class="yiiLog" width="100%" cellpadding="2" style="border-spacing:1px;font:11px Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;color:#666666;">
 	<tr>
 		<th style="background:black;color:white;" colspan="6">
 			Profiling Summary Report
