d0b7fed39acc8868bf118eda3d1716fe51dc2360
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-10-14 19:54:48 +0000

    (Fixes issue 600)

--- framework/web/CHttpSession.php
+++ framework/web/CHttpSession.php
@@ -95,12 +95,9 @@ class CHttpSession extends CApplicationComponent implements IteratorAggregate,Ar
 	 */
 	public function open()
 	{
-		if(session_id()==='')
-		{
-			if($this->getUseCustomStorage())
-				session_set_save_handler(array($this,'openSession'),array($this,'closeSession'),array($this,'readSession'),array($this,'writeSession'),array($this,'destroySession'),array($this,'gcSession'));
-			session_start();
-		}
+		if($this->getUseCustomStorage())
+			@session_set_save_handler(array($this,'openSession'),array($this,'closeSession'),array($this,'readSession'),array($this,'writeSession'),array($this,'destroySession'),array($this,'gcSession'));
+		@session_start();
 	}
 
 	/**
