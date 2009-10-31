b6d46ef69bb26b09ca7c149a4aa68fe1327458f6
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2009-10-31 13:57:40 +0000

    fixed name and id resolution.

--- framework/web/widgets/CMaskedTextField.php
+++ framework/web/widgets/CMaskedTextField.php
@@ -56,9 +56,13 @@ class CMaskedTextField extends CInputWidget
 	public function run()
 	{
 		list($name,$id)=$this->resolveNameID();
-		if(!isset($this->htmlOptions['id']))
+		if(isset($this->htmlOptions['id']))
+			$id=$this->htmlOptions['id'];
+		else
 			$this->htmlOptions['id']=$id;
-		if(!isset($this->htmlOptions['name']))
+		if(isset($this->htmlOptions['name']))
+			$name=$this->htmlOptions['name'];
+		else
 			$this->htmlOptions['name']=$name;
 
 		$this->registerClientScript();
