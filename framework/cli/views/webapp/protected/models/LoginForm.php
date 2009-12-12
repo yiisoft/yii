ab9170c9f5177947deccb907e02cba8a97a94fb5
qiang.xue <qiang.xue@gmail.com>
2009-12-12 17:48:23 +0000

    (Fixes issue 724)
    git-svn-id: http://yii.googlecode.com/svn/trunk@1577 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/models/LoginForm.php
+++ framework/cli/views/webapp/protected/models/LoginForm.php
@@ -21,6 +21,8 @@ class LoginForm extends CFormModel
 		return array(
 			// username and password are required
 			array('username, password', 'required'),
+			// rememberMe needs to be a boolean
+			array('rememberMe', 'boolean'),
 			// password needs to be authenticated
 			array('password', 'authenticate'),
 		);
