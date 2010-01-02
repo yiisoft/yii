e325794479d9aebd9feebb7b6c14301d63cdc120
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-01-02 22:59:59 +0000

    Added about page to webapp.

---
+++ framework/cli/views/webapp/protected/views/site/pages/about.php
@@ -0,0 +1,10 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - About';
+$this->breadcrumbs=array(
+	'About',
+);
+?>
+<h1>About</h1>
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <tt><?php echo __FILE__; ?></tt>.</p>
