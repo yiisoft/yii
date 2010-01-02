71c9d2e5864924cc1845ee04b864dbbb5621a0f0
qiang.xue <qiang.xue@gmail.com>
2010-01-02 22:59:59 +0000

    Added about page to webapp.
    git-svn-id: http://yii.googlecode.com/svn/trunk@1651 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/controllers/SiteController.php
+++ framework/cli/views/webapp/protected/controllers/SiteController.php
@@ -13,6 +13,11 @@ class SiteController extends Controller
 				'class'=>'CCaptchaAction',
 				'backColor'=>0xFFFFFF,
 			),
+			// page action renders "static" pages stored under 'protected/views/site/pages'
+			// They can be accessed via: index.php?r=site/page&view=FileName
+			'page'=>array(
+				'class'=>'CViewAction',
+			),
 		);
 	}
 
--- framework/cli/views/webapp/protected/views/layouts/main.php
+++ framework/cli/views/webapp/protected/views/layouts/main.php
@@ -29,6 +29,7 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
