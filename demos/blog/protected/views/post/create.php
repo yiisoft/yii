27bfa136b5b0a5ee4a6305531485c5a6abf88852
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-01-04 18:27:35 +0000

    removed old blog demo.

--- demos/blog/protected/views/post/create.php
+++
@@ -1,6 +0,0 @@
-<h2>Create New Post</h2>
-
-<?php $this->renderPartial('_form', array(
-	'post'=>$post,
-	'update'=>false,
-)); ?>
--- demos/blog/protected/views/post/update.php
+++
@@ -1,6 +0,0 @@
-<h2>Update Post <?php echo CHtml::link('#'.$post->id, array('post/show','id'=>$post->id)); ?></h2>
-
-<?php $this->renderPartial('_form', array(
-	'post'=>$post,
-	'update'=>true,
-)); ?>
