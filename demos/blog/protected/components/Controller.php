ec8e7f68c31b2174efe2c204a92ab0d47a059b6a
qiang.xue <qiang.xue@gmail.com>
2010-01-21 18:41:17 +0000

    git-svn-id: http://yii.googlecode.com/svn/trunk@1734 c7f931c7-7552-0410-b027-2fb3d89d9100

--- demos/blog/protected/components/Controller.php
+++ demos/blog/protected/components/Controller.php
@@ -6,6 +6,15 @@
 class Controller extends CController
 {
 	/**
+	 * @var string the default layout for the controller view. Defaults to 'column1',
+	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+	 */
+	public $layout='column1';
+	/**
+	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
+	 */
+	public $menu=array();
+	/**
 	 * @var array the breadcrumbs of the current page. The value of this property will
 	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
 	 * for more details on how to specify this property.
