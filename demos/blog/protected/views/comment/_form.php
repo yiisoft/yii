df6c57924f8c2f3ffc4f8dbc1070e4763777ecbc
qiang.xue <qiang.xue@gmail.com>
2010-01-25 18:25:03 +0000

    updated blog demo and tutorial to make use CActiveForm.
    git-svn-id: http://yii.googlecode.com/svn/trunk@1753 c7f931c7-7552-0410-b027-2fb3d89d9100

--- demos/blog/protected/views/comment/_form.php
+++ demos/blog/protected/views/comment/_form.php
@@ -1,30 +1,33 @@
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array('id'=>'comment-form')); ?>
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'comment-form',
+	'enableAjaxValidation'=>true,
+)); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'author'); ?>
-		<?php echo CHtml::activeTextField($model,'author',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->labelEx($model,'author'); ?>
+		<?php echo $form->textField($model,'author',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'author'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'email'); ?>
-		<?php echo CHtml::activeTextField($model,'email',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'email'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'url'); ?>
-		<?php echo CHtml::activeTextField($model,'url',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->labelEx($model,'url'); ?>
+		<?php echo $form->textField($model,'url',array('size'=>60,'maxlength'=>128)); ?>
 		<?php echo $form->error($model,'url'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'content'); ?>
-		<?php echo CHtml::activeTextArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->labelEx($model,'content'); ?>
+		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'content'); ?>
 	</div>
 
--- demos/blog/protected/views/site/login.php
+++ demos/blog/protected/views/site/login.php
@@ -10,35 +10,35 @@ $this->breadcrumbs=array(
 <p>Please fill out the following form with your login credentials:</p>
 
 <div class="form">
-<?php echo CHtml::beginForm(); ?>
+<?php $form=$this->beginWidget('CActiveForm'); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo CHtml::errorSummary($model); ?>
+	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'username'); ?>
-		<?php echo CHtml::activeTextField($model,'username'); ?>
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'password'); ?>
-		<?php echo CHtml::activePasswordField($model,'password'); ?>
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password'); ?>
 		<p class="hint">
 			Hint: You may login with <tt>demo/demo</tt>.
 		</p>
 	</div>
 
 	<div class="row rememberMe">
-		<?php echo CHtml::activeCheckBox($model,'rememberMe'); ?>
-		<?php echo CHtml::activeLabel($model,'rememberMe'); ?>
+		<?php echo $form->checkBox($model,'rememberMe'); ?>
+		<?php echo $form->label($model,'rememberMe'); ?>
 	</div>
 
 	<div class="row submit">
 		<?php echo CHtml::submitButton('Login'); ?>
 	</div>
 
-<?php echo CHtml::endForm(); ?>
+<?php $this->endWidget(); ?>
 </div><!-- form -->
 
 <?php
