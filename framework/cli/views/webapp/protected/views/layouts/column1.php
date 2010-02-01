4ed4aab53d00a5cf66bcd664f06c458f5b29b086
qiang.xue <qiang.xue@gmail.com>
2010-02-01 01:09:16 +0000

    (Fixes issue 871)
    git-svn-id: http://yii.googlecode.com/svn/trunk@1767 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/views/layouts/column1.php
+++ framework/cli/views/webapp/protected/views/layouts/column1.php
@@ -1,4 +1,4 @@
-<?php $this->beginContent('/layouts/main'); ?>
+<?php $this->beginContent('application.views.layouts.main'); ?>
 <div class="container">
 	<div id="content">
 		<?php echo $content; ?>
