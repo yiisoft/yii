cfc7c46121453438651db9e6508f17bf3bdd7f12
qiang.xue <qiang.xue@gmail.com>
2010-02-01 20:27:47 +0000

    git-svn-id: http://yii.googlecode.com/svn/trunk@1779 c7f931c7-7552-0410-b027-2fb3d89d9100

--- demos/blog/index-test.php
+++ demos/blog/index-test.php
@@ -9,7 +9,7 @@ $yii=dirname(__FILE__).'/../../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
 // remove the following line when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+// defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
