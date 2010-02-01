3721c5d7ffd46f714cf11a55e3639b6ca0bc8b62
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-02-01 01:09:16 +0000

    (Fixes issue 871)

--- framework/cli/views/webapp/protected/components/Controller.php
+++ framework/cli/views/webapp/protected/components/Controller.php
@@ -6,10 +6,10 @@
 class Controller extends CController
 {
 	/**
-	 * @var string the default layout for the controller view. Defaults to 'column1',
+	 * @var string the default layout for the controller view. Defaults to 'application.views.layouts.column1',
 	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
 	 */
-	public $layout='column1';
+	public $layout='application.views.layouts.column1';
 	/**
 	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
 	 */
