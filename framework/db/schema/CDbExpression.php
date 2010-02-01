206d1bdf2013c0eed579ebd848f9eefce3019084
qiang.xue <qiang.xue@gmail.com>
2010-02-01 23:02:40 +0000

    (Fixes issue 726)
    git-svn-id: http://yii.googlecode.com/svn/trunk@1787 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/db/schema/CDbExpression.php
+++ framework/db/schema/CDbExpression.php
@@ -17,6 +17,10 @@
  * expression so that saving the record would fill the corresponding column
  * with the current DB server timestamp.
  *
+ * Starting from version 1.1.1, one can also specify parameters to be bound
+ * for the expression. For example, if the expression is 'LOWER(:value)', then
+ * one can set {@link params} to be <code>array(':value'=>$value)</code>.
+ *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @version $Id$
  * @package system.db.schema
@@ -28,14 +32,22 @@ class CDbExpression extends CComponent
 	 * @var string the DB expression
 	 */
 	public $expression;
+	/**
+	 * @var array list of parameters that should be bound for this expression.
+	 * The keys are placeholders appearing in {@link expression}, while the values
+	 * are the corresponding parameter values.
+	 * @since 1.1.1
+	 */
+	public $params=array();
 
 	/**
 	 * Constructor.
 	 * @param string the DB expression
 	 */
-	public function __construct($expression)
+	public function __construct($expression,$params=array())
 	{
 		$this->expression=$expression;
+		$this->params=$params;
 	}
 
 	/**
