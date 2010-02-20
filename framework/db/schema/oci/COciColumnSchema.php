863c0a44dd88933f57bacc3add9443c87bd51af6
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-02-20 00:39:27 +0000

    (Fixes issue 738)

--- framework/db/schema/oci/COciColumnSchema.php
+++ framework/db/schema/oci/COciColumnSchema.php
@@ -25,20 +25,23 @@ class COciColumnSchema extends CDbColumnSchema
 	protected function extractOraType($dbType){
 		if(strpos($dbType,'FLOAT')!==false) return 'double';
 
-		if ((strpos($dbType,'NUMBER')!==false) or
-			(strpos($dbType,'INTEGER')!==false))
+		if (strpos($dbType,'NUMBER')!==false || strpos($dbType,'INTEGER')!==false)
 		{
 			if(strpos($dbType,'(') && preg_match('/\((.*)\)/',$dbType,$matches))
 			{
 				$values=explode(',',$matches[1]);
 				if(isset($values[1]) and (((int)$values[1]) > 0))
 					return 'double';
-				else return 'integer';
+				else
+					return 'integer';
 			}
-		}else{
-			return 'string';
+			else
+				return 'double';
 		}
+		else
+			return 'string';
 	}
+
 	protected function extractType($dbType)
 	{
 		$this->type=$this->extractOraType($dbType);
