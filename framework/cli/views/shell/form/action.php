05f934ffd4c7aa293db61e628ba7d2bdf12b96a6
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-03-10 22:47:03 +0000

    Added scenario parameter to the form command.

--- framework/cli/views/shell/form/action.php
+++ framework/cli/views/shell/form/action.php
@@ -9,29 +9,29 @@
 <?php
 $actionName=$modelClass;
 if(strrpos($modelClass,'Form')===strlen($modelClass)-4)
-	$actionName=substr($modelClass,0,strlen($modelClass)-4);
+    $actionName=substr($modelClass,0,strlen($modelClass)-4);
 ?>
 public function action<?php echo $actionName; ?>()
 {
-	$model=new <?php echo $modelClass; ?>;
+    $model=new <?php echo $modelClass; ?>;
 
-	// uncomment the following code to enable ajax-based validation
-	/*
-	if(isset($_POST['ajax']) && $_POST['ajax']==='<?php echo $this->class2id($modelClass); ?>-form')
-	{
-		echo CActiveForm::validate($model);
-		Yii::app()->end();
-	}
-	*/
+    // uncomment the following code to enable ajax-based validation
+    /*
+    if(isset($_POST['ajax']) && $_POST['ajax']==='<?php echo $this->class2id($modelClass); ?>-form')
+    {
+        echo CActiveForm::validate($model);
+        Yii::app()->end();
+    }
+    */
 
-	if(isset($_POST['<?php echo $modelClass; ?>']))
-	{
-		$model->attributes=$_POST['<?php echo $modelClass; ?>'];
-		if($model->validate())
-		{
-			// form inputs are valid, do something here
-			return;
-		}
-	}
-	$this->render('<?php echo $viewName; ?>',array('model'=>$model));
+    if(isset($_POST['<?php echo $modelClass; ?>']))
+    {
+        $model->attributes=$_POST['<?php echo $modelClass; ?>'];
+        if($model->validate())
+        {
+            // form inputs are valid, do something here
+            return;
+        }
+    }
+    $this->render('<?php echo $viewName; ?>',array('model'=>$model));
 }
