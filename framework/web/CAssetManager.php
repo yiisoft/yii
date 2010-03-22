47af1adb086fd402115d6b05b0391a4188741e17
qiang.xue <qiang.xue@gmail.com>
2010-03-22 19:26:01 +0000

    * Added $forceCopy parameter to CAssetManager::publish()
    git-svn-id: http://yii.googlecode.com/svn/trunk@1948 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/web/CAssetManager.php
+++ framework/web/CAssetManager.php
@@ -120,10 +120,15 @@ class CAssetManager extends CApplicationComponent
 	 * Level -1 means publishing all subdirectories and files;
 	 * Level 0 means publishing only the files DIRECTLY under the directory;
 	 * level N means copying those directories that are within N levels.
+	 * @param boolean whether we should copy the asset file or directory even if it is already published before.
+	 * This parameter is set true mainly during development stage when the original
+	 * assets are being constantly changed. The consequence is that the performance
+	 * is degraded, which is not a concern during development, however.
+	 * This parameter has been available since version 1.1.2.
 	 * @return string an absolute URL to the published asset
 	 * @throws CException if the asset to be published does not exist.
 	 */
-	public function publish($path,$hashByName=false,$level=-1)
+	public function publish($path,$hashByName=false,$level=-1,$forceCopy=false)
 	{
 		if(isset($this->_published[$path]))
 			return $this->_published[$path];
@@ -136,7 +141,7 @@ class CAssetManager extends CApplicationComponent
 				$dstDir=$this->getBasePath().DIRECTORY_SEPARATOR.$dir;
 				$dstFile=$dstDir.DIRECTORY_SEPARATOR.$fileName;
 
-				if(@filemtime($dstFile)<@filemtime($src))
+				if(@filemtime($dstFile)<@filemtime($src) || $forceCopy)
 				{
 					if(!is_dir($dstDir))
 					{
@@ -153,7 +158,7 @@ class CAssetManager extends CApplicationComponent
 				$dir=$this->hash($hashByName ? basename($src) : $src);
 				$dstDir=$this->getBasePath().DIRECTORY_SEPARATOR.$dir;
 
-				if(!is_dir($dstDir))
+				if(!is_dir($dstDir) || $forceCopy)
 					CFileHelper::copyDirectory($src,$dstDir,array('exclude'=>array('.svn'),'level'=>$level));
 
 				return $this->_published[$path]=$this->getBaseUrl().'/'.$dir;
