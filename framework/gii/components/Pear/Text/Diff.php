21a132095b83baccd725d000c36e0e2d9250608b
poppitypop <poppitypop@gmail.com>
2010-03-25 17:44:20 +0000

    made Text_Diff php 5.3 compatible
    git-svn-id: http://yii.googlecode.com/svn/trunk@1965 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/gii/components/Pear/Text/Diff.php
+++ framework/gii/components/Pear/Text/Diff.php
@@ -380,7 +380,7 @@ class Text_Diff_Op_copy extends Text_Diff_Op {
 
     function &reverse()
     {
-        $reverse = &new Text_Diff_Op_copy($this->final, $this->orig);
+        $reverse = new Text_Diff_Op_copy($this->final, $this->orig);
         return $reverse;
     }
 
@@ -402,7 +402,7 @@ class Text_Diff_Op_delete extends Text_Diff_Op {
 
     function &reverse()
     {
-        $reverse = &new Text_Diff_Op_add($this->orig);
+        $reverse = new Text_Diff_Op_add($this->orig);
         return $reverse;
     }
 
@@ -424,7 +424,7 @@ class Text_Diff_Op_add extends Text_Diff_Op {
 
     function &reverse()
     {
-        $reverse = &new Text_Diff_Op_delete($this->final);
+        $reverse = new Text_Diff_Op_delete($this->final);
         return $reverse;
     }
 
@@ -446,7 +446,7 @@ class Text_Diff_Op_change extends Text_Diff_Op {
 
     function &reverse()
     {
-        $reverse = &new Text_Diff_Op_change($this->final, $this->orig);
+        $reverse = new Text_Diff_Op_change($this->final, $this->orig);
         return $reverse;
     }
 
