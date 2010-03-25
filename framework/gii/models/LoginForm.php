b48c75486b3945e18db66c87932ff684f7e570fd
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-03-25 04:10:37 +0000

    css fix.

--- framework/gii/models/LoginForm.php
+++ framework/gii/models/LoginForm.php
@@ -5,34 +5,14 @@ Yii::import('gii.components.UserIdentity');
 class LoginForm extends CFormModel
 {
 	public $password;
-	public $rememberMe;
 
 	private $_identity;
 
-	/**
-	 * Declares the validation rules.
-	 * The rules state that username and password are required,
-	 * and password needs to be authenticated.
-	 */
 	public function rules()
 	{
 		return array(
-			// username and password are required
 			array('password', 'required'),
-			// rememberMe needs to be a boolean
-			array('rememberMe', 'boolean'),
-			// password needs to be authenticated
 			array('password', 'authenticate'),
-		);
-	}
-
-	/**
-	 * Declares attribute labels.
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'rememberMe'=>'Remember me next time',
 		);
 	}
 
@@ -48,7 +28,7 @@ class LoginForm extends CFormModel
 	}
 
 	/**
-	 * Logs in the user using the given username and password in the model.
+	 * Logs in the user using the given password in the model.
 	 * @return boolean whether login is successful
 	 */
 	public function login()
@@ -60,8 +40,7 @@ class LoginForm extends CFormModel
 		}
 		if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
 		{
-			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
-			Yii::app()->user->login($this->_identity,$duration);
+			Yii::app()->user->login($this->_identity);
 			return true;
 		}
 		else
