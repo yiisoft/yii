23455c3dfea162cae1641e2d233e3d26658b942c
qiang.xue <qiang.xue@gmail.com>
2010-04-03 02:19:50 +0000

    git-svn-id: http://yii.googlecode.com/svn/trunk@2001 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/gii/generators/form/templates/default/action.php
+++ framework/gii/generators/form/templates/default/action.php
@@ -9,11 +9,11 @@ $viewName=basename($this->viewName);
 ?>
 public function action<?php echo ucfirst(trim($viewName,'_')); ?>()
 {
-    $model=new <?php echo $this->modelClass; ?>;
+    $model=new <?php echo $this->modelClass; ?><?php echo empty($this->scenario) ? '' : "('{$this->scenario}')"; ?>;
 
     // uncomment the following code to enable ajax-based validation
     /*
-    if(isset($_POST['ajax']) && $_POST['ajax']==='<?php echo $this->class2id($this->modelClass); ?>-form')
+    if(isset($_POST['ajax']) && $_POST['ajax']==='<?php echo $this->class2id($this->modelClass); ?>-<?php echo $viewName; ?>-form')
     {
         echo CActiveForm::validate($model);
         Yii::app()->end();
