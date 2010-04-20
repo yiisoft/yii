0b6ada2d244d2063842877ddf8d19d13284dcdd9
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-04-20 19:16:36 +0000

    (Fixes issue 1137)

--- framework/gii/generators/crud/templates/default/view.php
+++ framework/gii/generators/crud/templates/default/view.php
@@ -18,7 +18,7 @@ $this->menu=array(
 	array('label'=>'List <?php echo $this->modelClass; ?>', 'url'=>array('index')),
 	array('label'=>'Create <?php echo $this->modelClass; ?>', 'url'=>array('create')),
 	array('label'=>'Update <?php echo $this->modelClass; ?>', 'url'=>array('update', 'id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>)),
-	array('label'=>'Delete <?php echo $this->modelClass; ?>', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>),'confirm'=>'Are you sure to delete this item?')),
+	array('label'=>'Delete <?php echo $this->modelClass; ?>', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage <?php echo $this->modelClass; ?>', 'url'=>array('admin')),
 );
 ?>
