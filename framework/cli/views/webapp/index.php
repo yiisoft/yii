ba8fefc1b5bb7459ef26085b254c5771deb15b3b
qiang.xue <qiang.xue@gmail.com>
2010-05-07 22:12:51 +0000

    (Fixes issue 1032)
    git-svn-id: http://yii.googlecode.com/svn/trunk@2111 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/index.php
+++ framework/cli/views/webapp/index.php
@@ -6,6 +6,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
