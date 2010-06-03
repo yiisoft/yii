929bd77861bba7b973053a73553194ebd6083a0b
qiang.xue <qiang.xue@gmail.com>
2010-06-03 15:09:32 +0000

    Added zii translations in Chinese.
    git-svn-id: http://yii.googlecode.com/svn/trunk@2160 c7f931c7-7552-0410-b027-2fb3d89d9100

---
+++ framework/messages/zh_cn/zii.php
@@ -0,0 +1,21 @@
+<?php
+
+return array (
+  'Are you sure you want to delete this item?' => '确定要删除这条数据吗?',
+  'Delete' => '删除',
+  'Displaying {start}-{end} of {count} result(s).' => '第 {start}-{end} 条, 共 {count} 条.',
+  'Either "name" or "value" must be specified for CDataColumn.' => 'CDataColumn 必须指定 "name" 或 "value" 至少一项.',
+  'No results found.' => '没有找到数据.',
+  'Not set' => '未设置',
+  'Please specify the "attributes" property.' => '请指定 "attributes" 属性.',
+  'Please specify the "data" property.' => '请指定 "data" 属性.',
+  'Sort by: ' => '排序: ',
+  'The "dataProvider" property cannot be empty.' => '"dataProvider" 属性不能为空.',
+  'The attribute must be specified in the format of "Name:Type:Label", where "Type" and "Label" are optional.' => '属性格式 "Name:Type:Label", "Type" 和 "Label" 为可选项.',
+  'The column must be specified in the format of "Name:Type:Label", where "Type" and "Label" are optional.' => '栏位格式 "Name:Type:Label", "Type" 和 "Label" 为可选项.',
+  'The property "itemView" cannot be empty.' => '必须指定 "itemView" 属性.',
+  'Total {count} result(s).' => '共 {count} 条.',
+  'Update' => '更新',
+  'View' => '查看',
+  '{class} must specify "model" and "attribute" or "name" property values.' => '{class} 必须给定 "model" 与 "attribute" 或 "name" 属性值.',
+);
