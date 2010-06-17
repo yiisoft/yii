4379a8570aad391a63994c94cd5d76708cf97c36
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-06-17 15:52:00 +0000

    (Fixes issue 1242)

--- framework/gii/generators/crud/templates/default/controller.php
+++ framework/gii/generators/crud/templates/default/controller.php
@@ -125,7 +125,7 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
 
 			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 			if(!isset($_GET['ajax']))
-				$this->redirect(array('index'));
+				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
 		}
 		else
 			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
