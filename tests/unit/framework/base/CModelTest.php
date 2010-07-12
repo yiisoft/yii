0e06e2bcfaac6ae55b306665edf7ded712200580
samdark <sam@rmcreative.ru>
2010-07-12 22:07:31 +0000

    more tests for CModel
    git-svn-id: http://yii.googlecode.com/svn/trunk@2261 c7f931c7-7552-0410-b027-2fb3d89d9100

--- tests/unit/framework/base/CModelTest.php
+++ tests/unit/framework/base/CModelTest.php
@@ -19,6 +19,39 @@ class CModelTest extends CTestCase
 		$this->assertTrue($model->validate());
 	}
 
+	function testBeforeValidate(){
+		$model=new NewModel;
+		$model->attr1=4;
+		$this->assertTrue($model->validate());
+
+		$model->onBeforeValidate = array($this, 'beforeValidate');
+		$this->assertFalse($model->validate());
+	}
+
+	function beforeValidate($event){
+		$event->isValid = false;
+	}
+
+	function testIsAttributeRequired(){
+		$model=new NewModel;
+		$this->assertTrue($model->isAttributeRequired('attr1'));
+		$this->assertFalse($model->isAttributeRequired('attr2'));
+	}	
+
+	function testIsAttributeSafe(){
+		$model=new NewModel;
+		$this->assertTrue($model->isAttributeSafe('attr1'));
+		$this->assertFalse($model->isAttributeSafe('attr3'));
+		$this->assertFalse($model->isAttributeSafe('attr4'));
+	}
+
+	function testGetSafeAttributeNames(){
+		$model=new NewModel;
+		$safeAttributes = $model->getSafeAttributeNames();
+		$this->assertContains('attr2', $safeAttributes);
+		$this->assertContains('attr1', $safeAttributes);
+	}
+
 	public function testModifyValidators()
 	{
 		$model=new NewModel;
@@ -39,5 +72,51 @@ class CModelTest extends CTestCase
 		$this->assertFalse($model->validate());
 		$model->attr2=3;
 		$this->assertTrue($model->validate());
+	}
+
+	function testErrors(){
+		$model=new NewModel;
+		$model->attr1=3;
+		$model->validatorList->add(CValidator::createValidator('required',$model,'attr2',array()));
+		$model->validatorList->add(CValidator::createValidator('required',$model,'attr4',array()));
+		$model->validate();
+
+		$this->assertTrue($model->hasErrors());
+		$this->assertTrue($model->hasErrors('attr2'));
+		$this->assertFalse($model->hasErrors('attr1'));
+
+		$model->clearErrors('attr2');
+		$this->assertFalse($model->hasErrors('attr2'));
+
+		$model->clearErrors();
+		$this->assertFalse($model->hasErrors());
+	}
+
+	function testGetAttributes(){
+		$model = new NewModel();
+		$model->attr1 = 1;
+		$model->attr2 = 2;
+
+		$attributes = $model->getAttributes();
+		$this->assertEquals(1, $attributes['attr1']);
+		$this->assertEquals(2, $attributes['attr2']);
+
+		$attributes = $model->getAttributes(array('attr1', 'non_existing'));
+		$this->assertEquals(1, $attributes['attr1']);
+		$this->assertEquals(null, $attributes['non_existing']);
+	}
+
+	function testUnsetAttributes(){
+		$model = new NewModel();
+		$model->attr1 = 1;
+		$model->attr2 = 2;
+
+		$model->unsetAttributes(array('attr1'));
+		$this->assertEquals(null, $model->attr1);
+		$this->assertEquals(2, $model->attr2);
+
+		$model->unsetAttributes();
+		$this->assertEquals(null, $model->attr1);
+		$this->assertEquals(null, $model->attr2);
 	}
 }
