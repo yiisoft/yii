ca8eaf9160f1fcb68a3d397438634f7920f7430f
qiang.xue <qiang.xue@gmail.com>
2010-08-29 23:21:33 +0000

    Added stricter checking.
    git-svn-id: http://yii.googlecode.com/svn/trunk@2370 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/web/actions/CInlineAction.php
+++ framework/web/actions/CInlineAction.php
@@ -31,24 +31,42 @@ class CInlineAction extends CAction
 		$controller=$this->getController();
 		$methodName='action'.$this->getId();
 		$method=new ReflectionMethod($controller,$methodName);
-		if($method->getNumberOfParameters()>0)
+		if(($n=$method->getNumberOfParameters())>0)
 		{
-			preg_match_all('/^\s*\**\s*@param\s+(\w+)\s*/m',$method->getDocComment(),$matches);
-			$types=$matches[1];
+			if(preg_match_all('/^\s*\**\s*@param\s+(\w+)\s+\$(\w+)/m',$method->getDocComment(),$matches))
+			{
+				$types=array_combine($matches[2],$matches[1]);
+				if($n!==count($types))
+				{
+					throw new CException(Yii::t('yii','The {method} has {count} parameters, but {count2} are declared in the method comment.',array(
+						'{method}'=>get_class($controller).'::'.$methodName.'()',
+						'{count}'=>$n,
+						'{count2}'=>count($types),
+					)));
+				}
+			}
 			$params=array();
 			foreach($method->getParameters() as $i=>$param)
 			{
 				$name=$param->getName();
+				if(isset($types) && !isset($types[$name]))
+				{
+					throw new CException(Yii::t('yii','The comment of {method} does not match its parameter declaration. Parameter {name} is not found in the method comment.',array(
+						'{method}'=>get_class($controller).'::'.$methodName.'()',
+						'{name}'=>'$'.$name,
+					)));
+				}
 				if(isset($_GET[$name]))
 				{
 					$value=$_GET[$name];
-					if(isset($types[$i]))
+					if(isset($types[$name]))
 					{
-						if($types[$i]==='integer' || $types[$i]==='int')
+						$type=$types[$name];
+						if($type==='integer' || $type==='int')
 							$value=(int)$value;
-						else if($types[$i]==='float' || $types[$i]==='double')
+						else if($type==='float' || $type==='double')
 							$value=(float)$value;
-						else if($types[$i]==='boolean' || $types[$i]==='bool')
+						else if($type==='boolean' || $type==='bool')
 							$value=(bool)$value;
 					}
 					$params[]=$value;
