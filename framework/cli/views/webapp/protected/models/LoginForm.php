261d91e3be0385489f034b63f75f93e66849bb49
qiang.xue <qiang.xue@gmail.com>
2010-09-28 19:39:10 +0000

    Fixed skipOnError bug in LoginForm.
    git-svn-id: http://yii.googlecode.com/svn/trunk@2509 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/models/LoginForm.php
+++ framework/cli/views/webapp/protected/models/LoginForm.php
@@ -26,7 +26,7 @@ class LoginForm extends CFormModel
 			// rememberMe needs to be a boolean
 			array('rememberMe', 'boolean'),
 			// password needs to be authenticated
-			array('password', 'authenticate', 'skipOnError'=>true),
+			array('password', 'authenticate'),
 		);
 	}
 
@@ -46,9 +46,12 @@ class LoginForm extends CFormModel
 	 */
 	public function authenticate($attribute,$params)
 	{
-		$this->_identity=new UserIdentity($this->username,$this->password);
-		if(!$this->_identity->authenticate())
-			$this->addError('password','Incorrect username or password.');
+		if(!$this->hasErrors())
+		{
+			$this->_identity=new UserIdentity($this->username,$this->password);
+			if(!$this->_identity->authenticate())
+				$this->addError('password','Incorrect username or password.');
+		}
 	}
 
 	/**
