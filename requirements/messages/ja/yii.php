873f5cf3af27feeadecf15af8bdd7a6f9428b6e7
mdomba <mdomba@gmail.com>
2010-09-30 19:08:33 +0000

    Fixes issue 1632
    git-svn-id: http://yii.googlecode.com/svn/trunk@2519 c7f931c7-7552-0410-b027-2fb3d89d9100

--- requirements/messages/ja/yii.php
+++ requirements/messages/ja/yii.php
@@ -13,9 +13,12 @@
  *
  * NOTE, this file must be saved in UTF-8 encoding.
  *
- * @version $Id: yii.php,v 1.2 2009-01-16 18:45:59+09 mocapapa Exp mocapapa $
+ * @version $Id: $
  */
 return array (
+  'GD extension with<br />FreeType support' => 'GD拡張(FreeTypeサポート)',
+  'GD installed<br />FreeType support not installed' => '',
+  'GD not installed' => '',
   '$_SERVER does not have {vars}.' => '$_SERVER は {vars} を持ちません。',
   '$_SERVER variable' => '$_SERVER 変数',
   '$_SERVER["SCRIPT_FILENAME"] must be the same as the entry script file path.' => '$_SERVER["SCRIPT_FILENAME"] はエントリスクリプトのパスと同じであるべきです。',
@@ -23,7 +26,6 @@ return array (
   'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>' => '全ての <a href="http://www.yiiframework.com/doc/api/#system.db"> データベース関連クラス</a>',
   'DOM extension' => 'DOM 拡張',
   'Either $_SERVER["REQUEST_URI"] or $_SERVER["QUERY_STRING"] must exist.' => '$_SERVER["REQUEST_URI"] か $_SERVER["QUERY_STRING"] が存在するべきです。',
-  'GD extension' => 'GD 拡張',
   'Mcrypt extension' => 'Mcrypt 拡張',
   'Memcache extension' => 'Memcache 拡張',
   'PCRE extension' => 'PCRE拡張',
@@ -41,6 +43,4 @@ return array (
   'This is required if you are using PostgreSQL database.' => 'これは PostgreSQL データベースをお使いなら必要です。',
   'This is required if you are using SQLite database.' => 'これは SQLite データベースをお使いなら必要です。',
   'Unable to determine URL path info. Please make sure $_SERVER["PATH_INFO"] (or $_SERVER["PHP_SELF"] and $_SERVER["SCRIPT_NAME"]) contains proper value.' => 'URL パス情報を決定できません。$_SERVER["PATH_INFO"] (もしくは $_SERVER["PHP_SELF"] と $_SERVER["SCRIPT_NAME"]) が正しい値かどうか確認してください。',
-  'GD installed<br />FreeType support not installed' => '',
-  'GD not installed' => '',
 );
