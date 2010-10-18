3240ca0e8b992332fb4dfeb6a946b20a5e165b2c
mdomba <mdomba@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-10-18 12:31:06 +0000

    Fixed doc for createUrl() return type, too

--- framework/web/CUrlManager.php
+++ framework/web/CUrlManager.php
@@ -607,7 +607,7 @@ class CUrlRule extends CComponent
 	 * @param string $route the route
 	 * @param array $params list of parameters
 	 * @param string $ampersand the token separating name-value pairs in the URL.
-	 * @return string the constructed URL
+	 * @return mixed the constructed URL or false on error
 	 */
 	public function createUrl($manager,$route,$params,$ampersand)
 	{
