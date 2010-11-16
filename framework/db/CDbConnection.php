0b3883783a09086ab8c90d724712ac404733e06d
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2010-11-16 22:33:30 +0000

    * Added query builder

--- framework/db/CDbConnection.php
+++ framework/db/CDbConnection.php
@@ -344,14 +344,17 @@ class CDbConnection extends CApplicationComponent
 
 	/**
 	 * Creates a command for execution.
-	 * @param string $sql SQL statement associated with the new command.
+	 * @param mixed $query the DB query to be executed. This can be either a string representing a SQL statement,
+	 * or an array representing different fragments of a SQL statement. Please refer to {@link CDbCommand::__construct}
+	 * for more details about how to pass an array as the query. If this parameter is not given,
+	 * you will have to call query builder methods of {@link CDbCommand} to build the DB query.
 	 * @return CDbCommand the DB command
 	 * @throws CException if the connection is not active
 	 */
-	public function createCommand($sql)
+	public function createCommand($query=null)
 	{
 		if($this->getActive())
-			return new CDbCommand($this,$sql);
+			return new CDbCommand($this,$query);
 		else
 			throw new CDbException(Yii::t('yii','CDbConnection is inactive and cannot perform any DB operations.'));
 	}
