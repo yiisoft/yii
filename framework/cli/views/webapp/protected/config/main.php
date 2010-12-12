4520808cee4e0b992c8876522489850b0d90e999
samdark <sam@rmcreative.ru>
2010-12-12 03:40:49 +0000

    (Fixes issue 1885) Added ipFilters to the Gii-created config file to reduce user confusion
    git-svn-id: http://yii.googlecode.com/svn/trunk@2738 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/config/main.php
+++ framework/cli/views/webapp/protected/config/main.php
@@ -24,6 +24,8 @@ return array(
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'Enter Your Password Here',
+		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		*/
 	),
