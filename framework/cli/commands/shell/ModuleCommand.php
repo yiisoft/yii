4be0af691723db10951e0f96224118c8bec655a8
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2011-01-01 19:31:13 +0000

    changed copyright year.

--- framework/cli/commands/shell/ModuleCommand.php
+++ framework/cli/commands/shell/ModuleCommand.php
@@ -4,7 +4,7 @@
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @link http://www.yiiframework.com/
- * @copyright Copyright &copy; 2008-2010 Yii Software LLC
+ * @copyright Copyright &copy; 2008-2011 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  * @version $Id: ModuleCommand.php 433 2008-12-30 22:59:17Z qiang.xue $
  */
