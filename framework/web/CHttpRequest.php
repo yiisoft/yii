da8a21149fa8d2ec87b36fe39963f1a2ae27abaa
mdomba <mdomba@gmail.com>
2011-01-06 18:48:53 +0000

    (Fixes issue 1909) Added CHttpRequest->xSendFile()
    git-svn-id: http://yii.googlecode.com/svn/trunk@2822 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/web/CHttpRequest.php
+++ framework/web/CHttpRequest.php
@@ -671,6 +671,90 @@ class CHttpRequest extends CApplicationComponent
 	}
 
 	/**
+     * Sends existing file to a browser as a download using x-sendfile.
+	 *
+	 * X-Sendfile is a feature allowing a web application to redirect the request for a file to the webserver
+	 * that in turn processes the request, this way eliminating the need to perform tasks like reading the file
+	 * and sending it to the user. When dealing with a lot of files (or very big files) this can lead to a great
+	 * increase in performance as the web application is allowed to terminate earlier while the webserver is
+	 * handling the request.
+	 *
+	 * The request is sent to the server through a special non-standard HTTP-header.
+	 * When the web server encounters the presence of such header it will discard all output and send the file
+	 * specified by that header instead using internals including all optimizations like caching-headers and sendfile.
+	 *
+	 * As this header directive is non-standard different directives exists for different web servers applications:
+	 * <ul>
+	 * <li>Apache: {@link http://tn123.org/mod_xsendfile X-Sendfile}</li>
+	 * <li>Lighttpd v1.4: {@link http://redmine.lighttpd.net/wiki/lighttpd/X-LIGHTTPD-send-file X-LIGHTTPD-send-file}</li>
+	 * <li>Lighttpd v1.5: X-Sendfile {@link http://redmine.lighttpd.net/wiki/lighttpd/X-LIGHTTPD-send-file X-LIGHTTPD-send-file}</li>
+	 * <li>Nginx: {@link http://wiki.nginx.org/XSendfile X-Accel-Redirect}</li>
+	 * <li>Cherokee: {@link http://www.cherokee-project.com/doc/other_goodies.html#x-sendfile X-Sendfile and X-Accel-Redirect}</li>
+	 * </ul>
+	 * So for this method to work the X-SENDFILE option/module should be enabled by the web server and
+	 * a proper xHeader should be sent.
+	 *
+	 * <b>Note:</b>
+	 * This option allows to download files that are not under web folders, and even files that are otherwise protected (deny from all) like .htaccess
+	 * 
+	 * <b>Side effects</b>:
+	 * If this option is disabled by the web server, when this method is called a download configuration dialog
+	 * will open but the downloaded file will have 0 bytes.
+	 *
+	 * <b>Example</b>:
+	 * <pre>
+	 * <?php
+	 *   Yii::app()->request->xSendFile('/home/user/Pictures/picture1.jpg',array(
+	 *      'saveName'=>'image1.jpg',
+	 *      'mimeType'=>'image/jpeg',
+	 *      'terminate'=>true,
+	 *   ));
+	 * ?>
+	 * </pre>
+     * @param string $filePath file name with full path
+     * @param array $options additional options:
+     * <ul>
+     * <li>saveName: file name shown to the user, if not set real file name will be used</li>
+     * <li>mimeType: mime type of the file, if not set it will be guessed automaticaly based on the file name.</li>
+     * <li>xHeader: appropriate x-sendfile header, defaults to "X-Sendfile"</li>
+     * <li>terminate: whether to terminate the current application after calling this method, defaults to false</li>
+     * </ul>
+     * @return boolean false if file not found, true otherwise.
+     */
+    public function xSendFile($filePath, $options=array())
+    {
+        if(!is_file($filePath))
+            return false;
+
+        if(!isset($options['saveName']))
+            $options['saveName']=basename($filePath);
+
+        if(!isset($options['mimeType']))
+        {
+            if(($options['mimeType']=CFileHelper::getMimeTypeByExtension($filePath))===null)
+                $options['mimeType']='text/plain';
+        }
+
+        if(!isset($options['xHeader']))
+            $options['xHeader']='X-Sendfile';
+
+        header('Content-type: '.$options['mimeType']);
+        header('Content-Disposition: attachment; filename="'.$options['saveName'].'"');
+        //header('Content-Length: '.filesize($filePath));
+        header('Content-Transfer-Encoding: binary');
+        header(trim($options['xHeader']).': '.$filePath);
+
+        if(isset($options['terminate']) && $options['terminate'])
+        {
+            // if Yii::app()->end() is used and eg CWebLogRoute is enabled that
+            // the "application log" would be added to the download content
+            //Yii::app()->end(0,false);
+            exit();
+        }
+		return true;
+    }
+
+	/**
 	 * Returns the random token used to perform CSRF validation.
 	 * The token will be read from cookie first. If not found, a new token
 	 * will be generated.
