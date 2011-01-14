b8da77e40b18b5caba2e1edbc4a6edb396a71d1b
alexander.makarow <alexander.makarow@c7f931c7-7552-0410-b027-2fb3d89d9100>
2011-01-14 21:37:13 +0000

    more tests for Yii::t

--- tests/unit/framework/i18n/data/ru/test.php
+++ tests/unit/framework/i18n/data/ru/test.php
@@ -6,4 +6,6 @@ return array(
 	'cucumber|cucumbers' => 'огурец|огурца|огурцов',
 	'{n} cucumber|{n} cucumbers' => '{n} огурец|{n} огурца|{n} огурцов',
 	'{sign} {n} cucumber|{sign} {n} cucumbers' => '{sign} {n} огурец|{sign} {n} огурца|{sign} {n} огурцов',
+	'zombie|zombies' => 'зомби',
+	'hat|hats' => 'шляпа|шляпы|шляп|useless1|useless2',
 );
