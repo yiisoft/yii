8211fb7d3eb9440844f31883824d5ae548b95f9e
qiang.xue <qiang.xue@gmail.com>
2011-02-11 03:48:01 +0000

    * Added support for query caching.
    git-svn-id: http://yii.googlecode.com/svn/trunk@2949 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/db/CDbCommand.php
+++ framework/db/CDbCommand.php
@@ -437,7 +437,24 @@ class CDbCommand extends CComponent
 		}
 		else
 			$par='';
+
 		Yii::trace('Querying SQL: '.$this->getText().$par,'system.db.CDbCommand');
+
+		if($this->_connection->queryCachingCount>0 && $method!==''
+				&& $this->_connection->queryCachingDuration>0
+				&& $this->_connection->queryCacheID!==false
+				&& ($cache=Yii::app()->getComponent($this->_connection->queryCacheID))!==null)
+		{
+			$this->_connection->queryCachingCount--;
+			$cacheKey='yii:dbquery'.$this->_connection->connectionString.':'.$this->_connection->username;
+			$cacheKey.=':'.$this->getText().':'.serialize(array_merge($this->_paramLog,$params));
+			if(($result=$cache->get($cacheKey))!==false)
+			{
+				Yii::trace('Query result found in cache','system.db.CDbCommand');
+				return $result;
+			}
+		}
+
 		try
 		{
 			if($this->_connection->enableProfiling)
@@ -459,6 +476,9 @@ class CDbCommand extends CComponent
 
 			if($this->_connection->enableProfiling)
 				Yii::endProfile('system.db.CDbCommand.query('.$this->getText().$par.')','system.db.CDbCommand.query');
+
+			if(isset($cache,$cacheKey))
+				$cache->set($cacheKey, $result, $this->_connection->queryCachingDuration, $this->_connection->queryCachingDependency);
 
 			return $result;
 		}
