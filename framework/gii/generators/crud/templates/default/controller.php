2af4dc57e7ed88a81b37f965eea27f03a62e78b2
samdark <sam@rmcreative.ru>
2011-02-18 21:26:28 +0000

    (Fixes issue 2113) Wrong CRUD action description
    git-svn-id: http://yii.googlecode.com/svn/trunk@2975 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/gii/generators/crud/templates/default/controller.php
+++ framework/gii/generators/crud/templates/default/controller.php
@@ -111,7 +111,7 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
 
 	/**
 	 * Deletes a particular model.
-	 * If deletion is successful, the browser will be redirected to the 'index' page.
+	 * If deletion is successful, the browser will be redirected to the 'admin' page.
 	 * @param integer $id the ID of the model to be deleted
 	 */
 	public function actionDelete($id)
