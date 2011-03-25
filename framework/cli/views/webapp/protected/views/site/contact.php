a91261549b2f1b45d58dd16bbf19f05f7c099baa
qiang.xue <qiang.xue@gmail.com>
2011-03-25 00:52:57 +0000

    updated skeleton code to make use client-side validation.
    git-svn-id: http://yii.googlecode.com/svn/trunk@3119 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/views/site/contact.php
+++ framework/cli/views/webapp/protected/views/site/contact.php
@@ -21,7 +21,13 @@ If you have business inquiries or other questions, please fill out the following
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm'); ?>
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'contact-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
@@ -30,21 +36,25 @@ If you have business inquiries or other questions, please fill out the following
 	<div class="row">
 		<?php echo $form->labelEx($model,'name'); ?>
 		<?php echo $form->textField($model,'name'); ?>
+		<?php echo $form->error($model,'name'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'email'); ?>
 		<?php echo $form->textField($model,'email'); ?>
+		<?php echo $form->error($model,'email'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'subject'); ?>
 		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+		<?php echo $form->error($model,'subject'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'body'); ?>
 		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'body'); ?>
 	</div>
 
 	<?php if(CCaptcha::checkRequirements()): ?>
@@ -56,6 +66,7 @@ If you have business inquiries or other questions, please fill out the following
 		</div>
 		<div class="hint">Please enter the letters as they are shown in the image above.
 		<br/>Letters are not case-sensitive.</div>
+		<?php echo $form->error($model,'verifyCode'); ?>
 	</div>
 	<?php endif; ?>
 
