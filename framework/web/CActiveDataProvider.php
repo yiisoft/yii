ac9687dc57b4cf2779e2d86da6c591cc6a21ea8a
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2011-04-04 20:59:09 +0000

    (Fixes issue 2292)

--- framework/web/CActiveDataProvider.php
+++ framework/web/CActiveDataProvider.php
@@ -127,14 +127,25 @@ class CActiveDataProvider extends CDataProvider
 			$pagination->applyLimit($criteria);
 		}
 
-		if(($sort=$this->getSort())!==false)
-			$sort->applyOrder($criteria);
-
 		$baseCriteria=$this->model->getDbCriteria(false);
-		if($baseCriteria!==null)
-			$baseCriteria=clone $baseCriteria;
+
+		if(($sort=$this->getSort())!==false)
+		{
+			// set model criteria so that CSort can use its table alias setting
+			if($baseCriteria!==null)
+			{
+				$c=clone $baseCriteria;
+				$c->mergeWith($criteria);
+				$this->model->setDbCriteria($c);
+			}
+			else
+				$this->model->setDbCriteria($criteria);
+			$sort->applyOrder($criteria);
+		}
+
+		$this->model->setDbCriteria($baseCriteria!==null ? clone $baseCriteria : null);
 		$data=$this->model->findAll($criteria);
-		$this->model->setDbCriteria($baseCriteria);
+		$this->model->setDbCriteria($baseCriteria);  // restore original criteria
 		return $data;
 	}
 
