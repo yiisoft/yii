c8d276d969b2a7f2625c6aeb8444cebe526b90ad
samdark <sam@rmcreative.ru>
2011-05-12 23:37:43 +0000

    additional test for email validator
    git-svn-id: http://yii.googlecode.com/svn/trunk@3214 c7f931c7-7552-0410-b027-2fb3d89d9100

--- tests/framework/validators/CEmailValidatorTest.php
+++ tests/framework/validators/CEmailValidatorTest.php
@@ -3,10 +3,17 @@ require 'ValidatorTestModel.php';
 
 class CEmailValidatorTest extends CTestCase
 {
-	function testEmpty()
+	public function testEmpty()
 	{
 		$model = new ValidatorTestModel();
 		$model->validate(array('email'));
 		$this->assertArrayHasKey('email', $model->getErrors());
+	}
+
+	public function testNumericEmail()
+	{
+		$emailValidator = new CEmailValidator();
+		$result = $emailValidator->validateValue("5011@gmail.com");
+		$this->assertTrue($result);
 	}
 }
