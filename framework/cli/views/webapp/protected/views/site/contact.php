36ad5621699628b5327dd7937692259e94847923
qiang.xue <qiang.xue@gmail.com>
2011-06-16 15:12:05 +0000

    reverted changes.
    git-svn-id: http://yii.googlecode.com/svn/trunk@3284 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/cli/views/webapp/protected/views/site/contact.php
+++ framework/cli/views/webapp/protected/views/site/contact.php
@@ -23,12 +23,10 @@ If you have business inquiries or other questions, please fill out the following
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'contact-form',
-	/*
 	'enableClientValidation'=>true,
 	'clientOptions'=>array(
 		'validateOnSubmit'=>true,
 	),
-	*/
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
