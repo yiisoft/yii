cf68064409b753bd9d9362b033829ff7b2dfb757
mdomba <mdomba@gmail.com>
2011-06-21 11:03:21 +0000

    Addition to r3274 - quoted field names, too
    git-svn-id: http://yii.googlecode.com/svn/trunk@3293 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/web/auth/schema-mssql.sql
+++ framework/web/auth/schema-mssql.sql
@@ -14,29 +14,29 @@ drop table if exists [AuthItem];
 
 create table [AuthItem]
 (
-   name                 varchar(64) not null,
-   type                 integer not null,
-   description          text,
-   bizrule              text,
-   data                 text,
-   primary key (name)
+   [name]                 varchar(64) not null,
+   [type]                 integer not null,
+   [description]          text,
+   [bizrule]              text,
+   [data]                 text,
+   primary key ([name])
 );
 
 create table [AuthItemChild]
 (
-   parent               varchar(64) not null,
-   child                varchar(64) not null,
-   primary key (parent,child),
-   foreign key (parent) references [AuthItem] (name) on delete cascade on update cascade,
-   foreign key (child) references [AuthItem] (name) on delete cascade on update cascade
+   [parent]               varchar(64) not null,
+   [child]                varchar(64) not null,
+   primary key ([parent],[child]),
+   foreign key ([parent]) references [AuthItem] ([name]) on delete cascade on update cascade,
+   foreign key ([child]) references [AuthItem] ([name]) on delete cascade on update cascade
 );
 
 create table [AuthAssignment]
 (
-   itemname             varchar(64) not null,
-   userid               varchar(64) not null,
-   bizrule              text,
-   data                 text,
-   primary key (itemname,userid),
-   foreign key (itemname) references [AuthItem] (name) on delete cascade on update cascade
+   [itemname]             varchar(64) not null,
+   [userid]               varchar(64) not null,
+   [bizrule]              text,
+   [data]                 text,
+   primary key ([itemname],[userid]),
+   foreign key ([itemname]) references [AuthItem] ([name]) on delete cascade on update cascade
 );
