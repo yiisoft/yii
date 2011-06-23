c198d4f5643c9beb6a7a7688e16825a48956a713
qiang.xue <qiang.xue@c7f931c7-7552-0410-b027-2fb3d89d9100>
2011-06-23 14:32:58 +0000

    (Fixes issue 2552)

--- framework/messages/config.php
+++ framework/messages/config.php
@@ -6,7 +6,7 @@
 return array(
 	'sourcePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'messagePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'messages',
-	'languages'=>array('zh_cn','zh_tw','de','el','es','sv','he','nl','pt','ru','it','fr','ja','pl','hu','ro','id','vi','bg','lv','sk'),
+	'languages'=>array('zh_cn','zh_tw','de','el','es','sv','he','nl','pt','pt_br','ru','it','fr','ja','pl','hu','ro','id','vi','bg','lv','sk'),
 	'fileTypes'=>array('php'),
     'overwrite'=>true,
 	'exclude'=>array(
