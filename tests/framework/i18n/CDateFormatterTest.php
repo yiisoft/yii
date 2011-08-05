a0989a047b90036b16079efd63e2a10047aeb600
samdark <sam@rmcreative.ru>
2011-08-05 22:24:27 +0000

    (Fixes issue 2717) Extracted MigrateCommand::createMigrationHistoryTable method from MigrateCommand::getMigrationHistory
    git-svn-id: http://yii.googlecode.com/svn/trunk@3371 c7f931c7-7552-0410-b027-2fb3d89d9100

---
+++ tests/framework/i18n/CDateFormatterTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CDateFormatterTest
+ */
+class CDateFormatterTest extends CTestCase
+{
+	public function testWeekInMonth()
+	{
+		for($year=1970; $year<=date('Y'); $year++)
+		{
+			for($month=1;$month<=12;$month++) {
+				$day=date('t',mktime(0,0,0,$month,1,$year));
+				$weekNum=Yii::app()->dateFormatter->format("W",mktime(0,0,0,$month,$day,$year));
+				echo sprintf("%d/%d/%d\t%d\n",$year,$month,$day,$weekNum);
+			}
+		}
+
+		echo "Week number for 2011/01/01 is ".Yii::app()->dateFormatter->format("W",mktime(0,0,0,1,1,2011));
+		echo "Week number for 2011/01/07 is ".Yii::app()->dateFormatter->format("W",mktime(0,0,0,1,7,2011));
+	}
+}
