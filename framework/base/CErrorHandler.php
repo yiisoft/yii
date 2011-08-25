d65d971a90a237e9eae53f2bf2a5e64f9fdce250
mdomba <mdomba@gmail.com>
2011-08-25 11:20:33 +0000

    removed checking for ajax calls in CErrorHandler::handleException() - was preventing to customize the display of the exception during an ajax call.
    git-svn-id: http://yii.googlecode.com/svn/trunk@3386 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/base/CErrorHandler.php
+++ framework/base/CErrorHandler.php
@@ -175,9 +175,8 @@ class CErrorHandler extends CApplicationComponent
 
 			if(!headers_sent())
 				header("HTTP/1.0 {$data['code']} ".get_class($exception));
-			if($this->isAjaxRequest())
-				$app->displayException($exception);
-			else if($exception instanceof CHttpException || !YII_DEBUG)
+
+			if($exception instanceof CHttpException || !YII_DEBUG)
 				$this->render('error',$data);
 			else
 				$this->render('exception',$data);
