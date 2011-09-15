9b713cf5036ae8df3fe440b171fea601b7ca6184
samdark <sam@rmcreative.ru>
2011-09-15 16:16:08 +0000

    (Fixes issue 2808) Added ability to override core classes using YiiBase::classMap
    git-svn-id: http://yii.googlecode.com/svn/trunk@3398 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/YiiBase.php
+++ framework/YiiBase.php
@@ -396,10 +396,10 @@ class YiiBase
 	public static function autoload($className)
 	{
 		// use include so that the error PHP file may appear
-		if(isset(self::$_coreClasses[$className]))
-			include(YII_PATH.self::$_coreClasses[$className]);
-		else if(isset(self::$classMap[$className]))
+		if(isset(self::$classMap[$className]))
 			include(self::$classMap[$className]);
+		else if(isset(self::$_coreClasses[$className]))
+			include(YII_PATH.self::$_coreClasses[$className]);
 		else
 		{
 			// include class file relying on include_path
