b10e82f79ef632908d84b7f8a024ee7f6a53f220
mdomba <mdomba@c7f931c7-7552-0410-b027-2fb3d89d9100>
2011-11-07 15:03:58 +0000

    jQuery fix: used prop() instead of attr() for the 'checked attribute'

--- framework/gii/assets/js/main.js
+++ framework/gii/assets/js/main.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
 	}
 
 	$('table.preview input[name="checkAll"]').click(function() {
-		$('table.preview .confirm input').attr('checked', this.checked);
+		$('table.preview .confirm input').prop('checked', this.checked);
 	});
 
 	$('table.preview td.confirm input').click(function() {
-		$('table.preview input[name="checkAll"]').attr('checked', !$('table.preview td.confirm input:not(:checked)').length);
+		$('table.preview input[name="checkAll"]').prop('checked', !$('table.preview td.confirm input:not(:checked)').length);
 	});
-	$('table.preview input[name="checkAll"]').attr('checked', !$('table.preview td.confirm input:not(:checked)').length);
+	$('table.preview input[name="checkAll"]').prop('checked', !$('table.preview td.confirm input:not(:checked)').length);
 
 	$('.form .row.sticky input:not(.error), .form .row.sticky select:not(.error), .form .row.sticky textarea:not(.error)').each(function(){
 		var value;
