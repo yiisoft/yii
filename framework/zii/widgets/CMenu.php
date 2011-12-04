acac2d578df25f512317377cbca877832f292eaf
alexander.makarow <alexander.makarow@c7f931c7-7552-0410-b027-2fb3d89d9100>
2011-12-04 02:07:41 +0000

    (Fixes issue 2920) More API docs for CMenu

--- framework/zii/widgets/CMenu.php
+++ framework/zii/widgets/CMenu.php
@@ -24,6 +24,7 @@
  *         // Important: you need to specify url as 'controller/action',
  *         // not just as 'controller' even if default acion is used.
  *         array('label'=>'Home', 'url'=>array('site/index')),
+ *         // 'Products' menu item will be selected no matter which tag parameter value is since it's not specified.
  *         array('label'=>'Products', 'url'=>array('product/index'), 'items'=>array(
  *             array('label'=>'New Arrivals', 'url'=>array('product/new', 'tag'=>'new')),
  *             array('label'=>'Most Popular', 'url'=>array('product/index', 'tag'=>'popular')),
