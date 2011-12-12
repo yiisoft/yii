9596096271cbc988e269ff6dc3cda14b50f65c60
mdomba <mdomba@gmail.com>
2011-12-12 14:52:45 +0000

    (Fixes issue 2890) added CInlineValidator::clientValidate
    git-svn-id: http://yii.googlecode.com/svn/trunk@3478 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/validators/CInlineValidator.php
+++ framework/validators/CInlineValidator.php
@@ -26,6 +26,10 @@ class CInlineValidator extends CValidator
 	 * @var array additional parameters that are passed to the validation method
 	 */
 	public $params;
+	/**
+	 * @var string the name of the method that returns the client validation code (See {@link clientValidateAttribute}).
+	 */
+	public $clientValidate;
 
 	/**
 	 * Validates the attribute of the object.
@@ -37,5 +41,38 @@ class CInlineValidator extends CValidator
 	{
 		$method=$this->method;
 		$object->$method($attribute,$this->params);
+	}
+
+	/**
+	 * Returns the JavaScript code needed to perform client-side validation by calling the {@link clientValidate} method.
+	 * In the client validation code, these variables are predefined:
+	 * <ul>
+	 * <li>value: the current input value associated with this attribute.</li>
+	 * <li>messages: an array that may be appended with new error messages for the attribute.</li>
+	 * <li>attribute: a data structure keeping all client-side options for the attribute</li>
+	 * </ul>
+	 * <b>Example</b>:
+	 * 
+	 * If {@link clientValidate} is set to "clientValidate123", clientValidate123() is the name of 
+	 * the method that returns the client validation code and can look like:
+	 * <pre>
+	 * <?php
+	 *   public function clientValidate123()
+	 *   {
+	 *      $js = "if(value != '123') { messages.push('Value should be 123'); }";
+	 *      return $js;
+	 *   }
+	 * ?>
+	 * </pre>
+	 	 * @param CModel $object the data object being validated
+	 * @param string $attribute the name of the attribute to be validated.
+	 * @return string the client-side validation script.
+	 * @see CActiveForm::enableClientValidation
+	 * @since 1.1.9
+	 */
+	public function clientValidateAttribute($object,$attribute)
+	{
+		$method=$this->clientValidate;
+		return $object->$method();
 	}
 }
