5ce6fb3316900486177c5bbc5e3956c49124773d
samdark <sam@rmcreative.ru>
2011-12-30 20:34:36 +0000

    Added command to check for BOM and optionally remove it, removed BOM from php files
    git-svn-id: http://yii.googlecode.com/svn/trunk@3524 c7f931c7-7552-0410-b027-2fb3d89d9100

--- framework/views/zh_tw/profile-summary.php
+++ framework/views/zh_tw/profile-summary.php
@@ -1,4 +1,4 @@
-﻿<!-- start profiling summary -->
+<!-- start profiling summary -->
 <table class="yiiLog" width="100%" cellpadding="2" style="border-spacing:1px;font:11px Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;color:#666666;">
 	<tr>
 		<th style="background:black;color:white;" colspan="6">
