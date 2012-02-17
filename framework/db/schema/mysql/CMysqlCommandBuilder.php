4ac67ea2239d9e8b654b6e9a1d2efd988abcf1db
DaSourcerer <webmaster@dasourcerer.net>
2012-02-17 13:01:06 +0100

    Add CMysqlCommandBuilder to handle joins on update
    Fixes issue #124
    CDbCommandBuilder produced faulty mysql update queries
    when joins are involved. JOIN has to come before SET in mysql.

---
+++ framework/db/schema/mysql/CMysqlCommandBuilder.php
@@ -0,0 +1,96 @@
+<?php
+class CMysqlCommandBuilder extends CDbCommandBuilder
+{
+	/**
+	 * Creates an UPDATE command.
+	 * @param mixed $table the table schema ({@link CDbTableSchema}) or the table name (string).
+	 * @param array $data list of columns to be updated (name=>value)
+	 * @param CDbCriteria $criteria the query criteria
+	 * @return CDbCommand update command.
+	 */
+	public function createUpdateCommand($table,$data,$criteria)
+	{
+		$this->ensureTable($table);
+		$fields=array();
+		$values=array();
+		$bindByPosition=isset($criteria->params[0]);
+		$i=0;
+		foreach($data as $name=>$value)
+		{
+			if(($column=$table->getColumn($name))!==null)
+			{
+				if($value instanceof CDbExpression)
+				{
+					$fields[]=$column->rawName.'='.$value->expression;
+					foreach($value->params as $n=>$v)
+						$values[$n]=$v;
+				}
+				else if($bindByPosition)
+				{
+					$fields[]=$column->rawName.'=?';
+					$values[]=$column->typecast($value);
+				}
+				else
+				{
+					$fields[]=$column->rawName.'='.self::PARAM_PREFIX.$i;
+					$values[self::PARAM_PREFIX.$i]=$column->typecast($value);
+					$i++;
+				}
+			}
+		}
+		if($fields===array())
+			throw new CDbException(Yii::t('yii','No columns are being updated for table "{table}".',
+				array('{table}'=>$table->name)));
+		$sql="UPDATE {$table->rawName}";
+		$sql=$this->applyJoin($sql,$criteria->join);
+		$sql.=" SET ".implode(', ',$fields);
+		$sql=$this->applyCondition($sql,$criteria->condition);
+		$sql=$this->applyOrder($sql,$criteria->order);
+		$sql=$this->applyLimit($sql,$criteria->limit,$criteria->offset);
+
+		$command=$this->getDbConnection()->createCommand($sql);
+		$this->bindValues($command,array_merge($values,$criteria->params));
+
+		return $command;
+	}
+	
+	/**
+	 * Creates an UPDATE command that increments/decrements certain columns.
+	 * @param mixed $table the table schema ({@link CDbTableSchema}) or the table name (string).
+	 * @param array $counters counters to be updated (counter increments/decrements indexed by column names.)
+	 * @param CDbCriteria $criteria the query criteria
+	 * @return CDbCommand the created command
+	 * @throws CException if no counter is specified
+	 */
+	public function createUpdateCounterCommand($table,$counters,$criteria)
+	{
+		$this->ensureTable($table);
+		$fields=array();
+		foreach($counters as $name=>$value)
+		{
+			if(($column=$table->getColumn($name))!==null)
+			{
+				$value=(int)$value;
+				if($value<0)
+					$fields[]="{$column->rawName}={$column->rawName}-".(-$value);
+				else
+					$fields[]="{$column->rawName}={$column->rawName}+".$value;
+			}
+		}
+		if($fields!==array())
+		{
+			$sql="UPDATE {$table->rawName}";
+			$sql=$this->applyJoin($sql,$criteria->join);
+			$sql.=" SET ".implode(', ',$fields);
+			$sql=$this->applyCondition($sql,$criteria->condition);
+			$sql=$this->applyOrder($sql,$criteria->order);
+			$sql=$this->applyLimit($sql,$criteria->limit,$criteria->offset);
+			$command=$this->getDbConnection()->createCommand($sql);
+			$this->bindValues($command,$criteria->params);
+			return $command;
+		}
+		else
+			throw new CDbException(Yii::t('yii','No counter columns are being updated for table "{table}".',
+				array('{table}'=>$table->name)));
+	}
+}
