2609587ace085a2c55f0a5a6b8c4ceae4b1727cf
resurtm <resurtm@gmail.com>
2012-02-17 11:01:51 +0600

    Changelog and small fix.

--- framework/logging/CWebLogRoute.php
+++ framework/logging/CWebLogRoute.php
@@ -68,7 +68,7 @@ class CWebLogRoute extends CLogRoute
 				return;
 			$view.='-firebug';
 		}
-		else if(!($app instanceof CWebApplication) || $isAjax)
+		else if(!($app instanceof CWebApplication) || $isAjax || $isFlash)
 			return;
 
 		$viewFile=YII_PATH.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.$view.'.php';
