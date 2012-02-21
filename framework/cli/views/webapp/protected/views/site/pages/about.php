e73a9d770da3f196c85441ba0676b7228de22654
Elvan Hidayat <evanhd@live.com>
2012-02-21 13:49:59 +0700

    Removed HTML5-deprecated <tt> tag

--- framework/cli/views/webapp/protected/views/site/pages/about.php
+++ framework/cli/views/webapp/protected/views/site/pages/about.php
@@ -7,4 +7,4 @@ $this->breadcrumbs=array(
 <h1>About</h1>
 
 <p>This is a "static" page. You may change the content of this page
-by updating the file <tt><?php echo __FILE__; ?></tt>.</p>
+by updating the file <code><?php echo __FILE__; ?></code>.</p>
