e9b70984a7427003f82df9b47a3dc83941592577
Zlatan Omerovic <reziraj@gmail.com>
2012-02-22 00:00:07 +0100

    381 Fixed auto-generation issues of the Model Class Name

--- framework/yiic.bat
+++ framework/yiic.bat
@@ -16,7 +16,7 @@ rem -------------------------------------------------------------
 
 set YII_PATH=%~dp0
 
-if "%PHP_COMMAND%" == "" set PHP_COMMAND=php.exe
+if "%PHP_COMMAND%" == "" set PHP_COMMAND=C:\xampp\php\php.exe
 
 "%PHP_COMMAND%" "%YII_PATH%yiic" %*
 
