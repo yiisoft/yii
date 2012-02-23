977997e66adf8dbb607057dfd8bf1b1731dec367
mdomba (mdlap) <mdomba@gmail.com>
2012-02-23 22:13:29 +0100

    proper tag for login info (kbd instead of code)

--- framework/cli/views/webapp/protected/views/site/login.php
+++ framework/cli/views/webapp/protected/views/site/login.php
@@ -31,7 +31,7 @@ $this->breadcrumbs=array(
 		<?php echo $form->passwordField($model,'password'); ?>
 		<?php echo $form->error($model,'password'); ?>
 		<p class="hint">
-			Hint: You may login with <code>demo/demo</code> or <code>admin/admin</code>.
+			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
 		</p>
 	</div>
 
