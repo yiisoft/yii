5ef06e73ad5ca103b5e1ac8f682a963360603bbd
mdomba (mdwork) <mdomba@gmail.com>
2012-03-23 10:08:22 +0100

    Issue #538 - additional fixes

--- build/commands/ApiCommand.php
+++ build/commands/ApiCommand.php
@@ -27,7 +27,7 @@ class ApiCommand extends CConsoleCommand
 	public $pageTitle;
 	public $themePath;
 	public $currentClass;
-	public $baseSourceUrl="https://github.com/yiisoft/yii/blob/master";
+	public $baseSourceUrl="https://github.com/yiisoft/yii/blob";
 	public $version;
 
 	public function getHelp()
@@ -104,13 +104,13 @@ EOD;
 		$this->version=Yii::getVersion();
 
 		/*
-		 * development version - link to framework
+		 * development version - link to master
 		 * release version link to tags
 		 */
 		if(substr($this->version,-3)=='dev')
-			$this->baseSourceUrl .= '/framework';
+			$this->baseSourceUrl .= '/master/framework';
 		else
-			$this->baseSourceUrl .= '/tags/'.$this->version.'/framework';
+			$this->baseSourceUrl .= '/'.$this->version.'/framework';
 
 		$this->pageTitle='Yii Framework Class Reference';
 		$themePath=dirname(__FILE__).'/api';
@@ -183,7 +183,7 @@ EOD;
 		if($line===null)
 			return CHtml::link('framework'.$sourcePath,$this->baseSourceUrl.$sourcePath,array('class'=>'sourceLink'));
 		else
-			return CHtml::link('framework'.$sourcePath.'#'.$line, $this->baseSourceUrl.$sourcePath.'#'.$line,array('class'=>'sourceLink'));
+			return CHtml::link('framework'.$sourcePath.'#'.$line, $this->baseSourceUrl.$sourcePath.'#L'.$line,array('class'=>'sourceLink'));
 	}
 
 	public function highlight($code,$limit=20)
