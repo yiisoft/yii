c1c87c8eaf9a54608b50148e79e6e78b7aeb9fb5
Charles Pick <charles.pick@gmail.com>
2012-03-23 10:58:08 +0000

    #545 reformat code according to Yii style

--- framework/web/CDataProviderIterator.php
+++ framework/web/CDataProviderIterator.php
@@ -20,7 +20,7 @@
  * $dataProvider = new CActiveDataProvider("User");
  * $iterator = new CDataProviderIterator($dataProvider);
  * foreach($iterator as $user) {
- * 	echo $user->name."\n";
+ *	 echo $user->name."\n";
  * }
  * </pre>
  *
@@ -37,55 +37,63 @@ class CDataProviderIterator implements Iterator, Countable
 
 	/**
 	 * The data provider to iterate over
+	 *
 	 * @var CDataProvider
 	 */
 	public $dataProvider;
 
 	/**
 	 * The current index
+	 *
 	 * @var integer
 	 */
-	private $_currentIndex = -1;
+	private $_currentIndex=-1;
 
 	/**
 	 * The current page in the pagination
+	 *
 	 * @var integer
 	 */
-	private $_currentPage = 0;
+	private $_currentPage=0;
 
 	/**
 	 * The total number of items
+	 *
 	 * @var integer
 	 */
-	private $_totalItems = -1;
+	private $_totalItems=-1;
 
 	/**
 	 * The current set of items
+	 *
 	 * @var array
 	 */
 	private $_items;
 
 	/**
 	 * Constructor. Sets the data provider to iterator over
+	 *
 	 * @param CDataProvider $dataProvider the data provider to iterate over
 	 */
 	public function __construct(CDataProvider $dataProvider)
 	{
-		$this->dataProvider = $dataProvider;
-		$this->_totalItems = $dataProvider->getTotalItemCount();
+		$this->dataProvider=$dataProvider;
+		$this->_totalItems=$dataProvider->getTotalItemCount();
 	}
 
 	/**
 	 * Sets the current index position
+	 *
 	 * @param integer $currentIndex the current index positon
 	 */
 	public function setCurrentIndex($currentIndex)
 	{
-		$this->_currentIndex = $currentIndex;
+		$this->_currentIndex=$currentIndex;
 	}
 
 	/**
 	 * Gets the zero based current index position
+	 *
 	 * @return integer index position
 	 */
 	public function getCurrentIndex()
@@ -95,6 +103,7 @@ class CDataProviderIterator implements Iterator, Countable
 
 	/**
 	 * Gets the current set of items to iterate over
+	 *
 	 * @return array the current set items to iterate over
 	 */
 	public function getItems()
@@ -104,15 +113,17 @@ class CDataProviderIterator implements Iterator, Countable
 
 	/**
 	 * Sets the total number of items to iterate over
+	 *
 	 * @param int $totalItems the total number of items to iterate over
 	 */
 	public function setTotalItems($totalItems)
 	{
-		$this->_totalItems = $totalItems;
+		$this->_totalItems=$totalItems;
 	}
 
 	/**
 	 * Gets the total number of items to iterate over
+	 *
 	 * @return integer the total number of items to iterate over
 	 */
 	public function getTotalItems()
@@ -122,16 +133,19 @@ class CDataProviderIterator implements Iterator, Countable
 
 	/**
 	 * Loads the page of results
+	 *
 	 * @return array the items from the next page of results
 	 */
 	protected function loadPage()
 	{
 		$this->dataProvider->getPagination()->setCurrentPage($this->_currentPage);
-		return $this->_items = $this->dataProvider->getData(true);
+		return $this->_items=$this->dataProvider->getData(true);
 	}
+
 	/**
 	 * Gets the current item in the list.
 	 * This method is required by the Iterator interface
+	 *
 	 * @return mixed the current item in the list
 	 */
 	public function current()
@@ -142,13 +156,14 @@ class CDataProviderIterator implements Iterator, Countable
 	/**
 	 * Gets the key of the current item.
 	 * This method is required by the Iterator interface
+	 *
 	 * @return integer the key of the current item
 	 */
 	public function key()
 	{
-		$pagination = $this->dataProvider->getPagination();
-		$pageSize = $pagination->getPageSize();
-		return $this->_currentPage * $pageSize + $this->_currentIndex;
+		$pagination=$this->dataProvider->getPagination();
+		$pageSize=$pagination->getPageSize();
+		return $this->_currentPage*$pageSize+$this->_currentIndex;
 	}
 
 	/**
@@ -157,11 +172,11 @@ class CDataProviderIterator implements Iterator, Countable
 	 */
 	public function next()
 	{
-		$pagination = $this->dataProvider->getPagination();
+		$pagination=$this->dataProvider->getPagination();
 		$this->_currentIndex++;
-		if ($this->_currentIndex >= $pagination->getPageSize()) {
+		if($this->_currentIndex>=$pagination->getPageSize()){
 			$this->_currentPage++;
-			$this->_currentIndex = 0;
+			$this->_currentIndex=0;
 			$this->loadPage();
 		}
 	}
@@ -172,24 +187,26 @@ class CDataProviderIterator implements Iterator, Countable
 	 */
 	public function rewind()
 	{
-		$this->_currentIndex = 0;
-		$this->_currentPage = 0;
+		$this->_currentIndex=0;
+		$this->_currentPage=0;
 		$this->loadPage();
 	}
 
 	/**
 	 * Checks if the current position is valid or not.
 	 * This method is required by the Iterator interface
+	 *
 	 * @return boolean true if this index is valid
 	 */
 	public function valid()
 	{
-		return $this->key() < $this->_totalItems;
+		return $this->key()<$this->_totalItems;
 	}
 
 	/**
 	 * Gets the total number of items in the dataProvider
 	 * This method is required by the Countable interface
+	 *
 	 * @return integer the total number of items
 	 */
 	public function count()
