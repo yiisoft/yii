e6a98c706566b15113f37331fb9b4a7e798f2044
resurtm <resurtm@gmail.com>
2012-05-26 13:27:32 +0600

    CHtml::resolveValue() ignoring of attribute elements accessor at the begining of the $attribute argument now works properly.

--- framework/web/helpers/CHtml.php
+++ framework/web/helpers/CHtml.php
@@ -2125,7 +2125,7 @@ EOD;
 		{
 			if($pos===0)  // [a]name[b][c], should ignore [a]
 			{
-				if(preg_match('/\](\w+)/',$attribute,$matches))
+				if(preg_match('/\](.*)/',$attribute,$matches))
 					$attribute=$matches[1];
 				if(($pos=strpos($attribute,'['))===false)
 					return $model->$attribute;
