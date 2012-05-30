yiisoft/yii

commit 44d5e842831521b38c0e8d652436019cd2719681

(fixes issue 1968) Fixed inconsistence in CActiveForm error highlighting when checkBoxList or radioButtonList are used

framework/web/helpers/CHtml.php
@@ -918,7 +918,7 @@ EOD;
 			$cs->registerScript($id,$js);
 		}
 
-		return implode($separator,$items);
+		return self::tag('span',array('id'=>$baseID),implode($separator,$items));
 	}
 
 	/**
@@ -963,7 +963,7 @@ EOD;
 			$label=self::label($label,$htmlOptions['id'],$labelOptions);
 			$items[]=strtr($template,array('{input}'=>$option,'{label}'=>$label));
 		}
-		return implode($separator,$items);
+		return self::tag('span',array('id'=>$baseID),implode($separator,$items));
 	}
 
 	/**

UPGRADE
@@ -27,6 +27,20 @@ Upgrading from v1.1.8
 - In CErrorHandler::handleException() the checking for ajax call has been removed
 as it was preventing to customize the display of the exception during an ajax call.
 
+- Previously in case of validation error the CSS "error" class was not added to the row container at all when 
+checkBoxList or radioButtonList where used. This is fixed now and proper "error" CSS class is added to the row
+container but in case of validation error, all labels from the list are shown in red because of the CSS rule.
+
+To fix this and display only the attribute label in red:
+
+in <projectdir>/css/form.css the line:
+
+     div.form div.error label
+
+should be changed to 
+
+     div.form div.error label:first-child
+
 - If you've used "through" ActiveRecord option in your relation definitions it's good to update code as shown below.
 Old style of defining this option still works but is now deprecated.
 

framework/cli/views/webapp/css/form.css
@@ -82,7 +82,7 @@ div.form span.required
 	color: red;
 }
 
-div.form div.error label,
+div.form div.error label:first-child,
 div.form label.error,
 div.form span.error
 {

framework/web/js/source/jquery.yiiactiveform.js
@@ -16,9 +16,9 @@
 	 */
 	$.fn.yiiactiveform = function(options) {
 		return this.each(function() {
-			var settings = $.extend({}, $.fn.yiiactiveform.defaults, options || {});
-			var $form = $(this);
-			var id = $form.attr('id');
+			var settings = $.extend({}, $.fn.yiiactiveform.defaults, options || {}),
+				$form = $(this);
+
 			if(settings.validationUrl == undefined)
 				settings.validationUrl = $form.attr('action');
 			$.each(settings.attributes, function(i,attribute){
@@ -82,8 +82,8 @@
 
 			$.each(settings.attributes, function(i, attribute) {
 				if (attribute.validateOnChange) {
-					$('#'+attribute.inputID, $form).change(function(){
-						validate(attribute, this.type=='checkbox' || this.type=='radio');
+					$('#'+attribute.inputID, $form).change(function(e){
+						validate(attribute, e.target.type=='checkbox' || e.target.type=='radio');
 					}).blur(function(){
 						if(attribute.status!=2 && attribute.status!=3)
 							validate(attribute, !attribute.status);
@@ -152,8 +152,8 @@
 				setTimeout(function(){
 					$.each(settings.attributes, function(i, attribute){
 						attribute.status = 0;
-						var $error = $('#'+attribute.errorID, $form);
-						var $container = $.fn.yiiactiveform.getInputContainer(attribute, $form);
+						var $error = $('#'+attribute.errorID, $form),
+							$container = $.fn.yiiactiveform.getInputContainer(attribute, $form);
 
 						$container.removeClass(
 							attribute.validatingCssClass + ' ' +
@@ -213,9 +213,10 @@
 	 */
 	$.fn.yiiactiveform.updateInput = function(attribute, messages, form) {
 		attribute.status = 1;
-		var hasError = messages!=null && $.isArray(messages[attribute.id]) && messages[attribute.id].length>0;
-		var $error = $('#'+attribute.errorID, form);
-		var $container = $.fn.yiiactiveform.getInputContainer(attribute, form);
+		var hasError = messages!=null && $.isArray(messages[attribute.id]) && messages[attribute.id].length>0,
+			$error = $('#'+attribute.errorID, form),
+			$container = $.fn.yiiactiveform.getInputContainer(attribute, form);
+
 		$container.removeClass(
 			attribute.validatingCssClass + ' ' + 
 			attribute.errorCssClass + ' ' + 
@@ -243,10 +244,10 @@
 	 * @param messages array the json data obtained from the ajax validation request
 	 */
 	$.fn.yiiactiveform.updateSummary = function(form, messages) {
-		var settings = $(form).data('settings');
+		var settings = $(form).data('settings'),
+			content = '';
 		if (settings.summaryID == undefined)
 			return;
-		var content = '';
 		$.each(settings.attributes, function(i, attribute){
 			if(messages && $.isArray(messages[attribute.id])) {
 				$.each(messages[attribute.id],function(j,message){
@@ -266,11 +267,10 @@
 	 * @param errorCallback function the function to be invoked if the ajax request fails
 	 */
 	$.fn.yiiactiveform.validate = function(form, successCallback, errorCallback) {
-		var $form = $(form);
-		var settings = $form.data('settings');
-
-		var messages = {};
-		var needAjaxValidation = false;
+		var $form = $(form),
+			settings = $form.data('settings'),
+			needAjaxValidation = false,
+			messages = {};
 		$.each(settings.attributes, function(){
 			var msg = [];
 			if (this.clientValidation != undefined && (settings.submitting || this.status == 2 || this.status == 3)) {
@@ -297,8 +297,8 @@
 			return;
 		}
 
-		var $button = $form.data('submitObject');
-		var extData = '&'+settings.ajaxVar+'='+$form.attr('id');
+		var $button = $form.data('submitObject'),
+			extData = '&'+settings.ajaxVar+'='+$form.attr('id');
 		if($button && $button.length)
 			extData += '&'+$button.attr('name')+'='+$button.attr('value');
 

commit 99de74373641e95387b9fd7bf7a9359d996a0111

Fix regression introduced in commit 44d5e8428

framework/web/helpers/CHtml.php
@@ -843,6 +843,8 @@ class CHtml
 	 * the checkbox list.</li>
 	 * <li>labelOptions: array, specifies the additional HTML attributes to be rendered
 	 * for every label tag in the list.</li>
+	 * <li>containerTag: string|false, specifies if the checkboxes should be enclosed in a tag. Defaults to span.
+	 * If the value is false, no enclosing tag will be generated</li>
 	 * </ul>
 	 * @return string the generated check box list
 	 */
@@ -850,7 +852,8 @@ class CHtml
 	{
 		$template=isset($htmlOptions['template'])?$htmlOptions['template']:'{input} {label}';
 		$separator=isset($htmlOptions['separator'])?$htmlOptions['separator']:"<br/>\n";
-		unset($htmlOptions['template'],$htmlOptions['separator']);
+		$containerTag=isset($htmlOptions['containerTag'])?$htmlOptions['containerTag']:'span';
+		unset($htmlOptions['template'],$htmlOptions['separator'],$htmlOptions['containerTag']);
 
 		if(substr($name,-2)!=='[]')
 			$name.='[]';
@@ -907,7 +910,10 @@ EOD;
 			$cs->registerScript($id,$js);
 		}
 
-		return self::tag('span',array('id'=>$baseID),implode($separator,$items));
+		if($containerTag===false)
+			return implode($separator,$items);
+		else
+			return self::tag($containerTag,array('id'=>$baseID),implode($separator,$items));
 	}
 
 	/**
@@ -928,6 +934,8 @@ EOD;
 	 * <li>separator: string, specifies the string that separates the generated radio buttons. Defaults to new line (<br/>).</li>
 	 * <li>labelOptions: array, specifies the additional HTML attributes to be rendered
 	 * for every label tag in the list.</li>
+	 * <li>containerTag: string|false, specifies if the radio buttons should be enclosed in a tag. Defaults to span.
+	 * If the value is false, no enclosing tag will be generated</li>
 	 * </ul>
 	 * @return string the generated radio button list
 	 */
@@ -935,7 +943,8 @@ EOD;
 	{
 		$template=isset($htmlOptions['template'])?$htmlOptions['template']:'{input} {label}';
 		$separator=isset($htmlOptions['separator'])?$htmlOptions['separator']:"<br/>\n";
-		unset($htmlOptions['template'],$htmlOptions['separator']);
+		$containerTag=isset($htmlOptions['containerTag'])?$htmlOptions['containerTag']:'span';
+		unset($htmlOptions['template'],$htmlOptions['separator'],$htmlOptions['containerTag']);
 
 		$labelOptions=isset($htmlOptions['labelOptions'])?$htmlOptions['labelOptions']:array();
 		unset($htmlOptions['labelOptions']);
@@ -952,7 +961,10 @@ EOD;
 			$label=self::label($label,$htmlOptions['id'],$labelOptions);
 			$items[]=strtr($template,array('{input}'=>$option,'{label}'=>$label));
 		}
-		return self::tag('span',array('id'=>$baseID),implode($separator,$items));
+		if($containerTag===false)
+			return implode($separator,$items);
+		else
+			return self::tag($containerTag,array('id'=>$baseID),implode($separator,$items));
 	}
 
 	/**