628acb485486eec20fe8eba2ca2b154bc6943547
resurtm <resurtm@gmail.com>
2012-06-28 18:02:33 +0600

    Fixed indentation issues in CDbSchema and CMysqlSchema.

--- framework/db/schema/mysql/CMysqlSchema.php
+++ framework/db/schema/mysql/CMysqlSchema.php
@@ -22,21 +22,21 @@ class CMysqlSchema extends CDbSchema
 	 * @var array the abstract column types mapped to physical column types.
 	 * @since 1.1.6
 	 */
-    public $columnTypes=array(
-        'pk' => 'int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY',
-        'string' => 'varchar(255)',
-        'text' => 'text',
-        'integer' => 'int(11)',
-        'float' => 'float',
-        'decimal' => 'decimal',
-        'datetime' => 'datetime',
-        'timestamp' => 'timestamp',
-        'time' => 'time',
-        'date' => 'date',
-        'binary' => 'blob',
-        'boolean' => 'tinyint(1)',
+	public $columnTypes=array(
+		'pk' => 'int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY',
+		'string' => 'varchar(255)',
+		'text' => 'text',
+		'integer' => 'int(11)',
+		'float' => 'float',
+		'decimal' => 'decimal',
+		'datetime' => 'datetime',
+		'timestamp' => 'timestamp',
+		'time' => 'time',
+		'date' => 'date',
+		'binary' => 'blob',
+		'boolean' => 'tinyint(1)',
 		'money' => 'decimal(19,4)',
-    );
+	);
 
 	/**
 	 * Quotes a table name for use in a query.
