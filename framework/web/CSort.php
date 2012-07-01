3a306014681a9d8e1d603bbdad2ade29572ce069
Alexander Makarov <sam@rmcreative.ru>
2012-07-02 02:33:13 +0400

    fixed PHPDoc

--- framework/web/CSort.php
+++ framework/web/CSort.php
@@ -225,9 +225,9 @@ class CSort extends CComponent
 	}
 
 	/**
+	 * @param CDbCriteria $criteria the query criteria
 	 * @return string the order-by columns represented by this sort object.
 	 * This can be put in the ORDER BY clause of a SQL statement.
-	 * @param CDbCriteria $criteria the query criteria
 	 * @since 1.1.0
 	 */
 	public function getOrderBy($criteria=null)
