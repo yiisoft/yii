33ff7ebd89b12212fc0b92be76514267e6bea286
resurtm <resurtm@gmail.com>
2012-07-03 19:12:48 +0600

    Indentation fixes were done in the following directories: console, collections, base, caching, cli.

--- framework/cli/views/webapp/protected/data/schema.mysql.sql
+++ framework/cli/views/webapp/protected/data/schema.mysql.sql
@@ -1,8 +1,8 @@
 CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(128) NOT NULL,
+	password VARCHAR(128) NOT NULL,
+	email VARCHAR(128) NOT NULL
 );
 
 INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
