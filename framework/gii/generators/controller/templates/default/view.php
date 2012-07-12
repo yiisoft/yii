4db0b7a0b95219326013296d3b93d5dc16b36990
resurtm <resurtm@gmail.com>
2012-07-12 19:09:19 +0600

    Gii default templates: view variables metadata (phpdoc) joined into a single php tag (<?php and ?>).

--- framework/gii/generators/controller/templates/default/view.php
+++ framework/gii/generators/controller/templates/default/view.php
@@ -10,10 +10,8 @@
 /**
  * @var $this <?php echo $this->getControllerClass()."\n"; ?>
  */
-?>
 
 <?php
-echo "<?php\n";
 $label=ucwords(trim(strtolower(str_replace(array('-','_','.'),' ',preg_replace('/(?<![A-Z])[A-Z]/', ' \0', basename($this->getControllerID()))))));
 if($action==='index')
 {
