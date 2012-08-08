c621bfeb513bcabaa9037fef5c4520c8b5cf67a9
mdomba (mdwork) <mdomba@gmail.com>
2012-08-08 15:01:48 +0200

    Issue #1099 - changed connectionID to sticky text field

--- framework/gii/CCodeGenerator.php
+++ framework/gii/CCodeGenerator.php
@@ -165,4 +165,34 @@ class CCodeGenerator extends CController
 		}
 		return $model;
 	}
+
+	/**
+	 * Provides autocomplete table names
+	 */
+	public function actionGetTableNames($db, $q)
+	{
+		if(Yii::app()->getRequest()->getIsAjaxRequest())
+		{
+			if(empty($db) || Yii::app()->hasComponent($db)===false || !(Yii::app()->getComponent($db) instanceof CDbConnection))
+				throw new CHttpException(404,'The database component is not valid.');
+
+			$model=$this->prepare();
+			$all=array_keys(Yii::app()->{$db}->schema->getTables());
+
+			if($q!=='')
+			{
+				$filtered=array();
+				foreach($all as $table)
+				{
+					if(stripos($table, $q) !== false)
+						$filtered[]=$table;
+				}
+				echo json_encode($filtered);
+			}
+			else
+				echo json_encode($all);
+		}
+		else
+			throw new CHttpException(404,'The requested page does not exist.');
+	}
 }
