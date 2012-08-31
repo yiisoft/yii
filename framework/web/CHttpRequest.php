f270eb763e1d8cf3f9734dec2834464d86e0ea34
Mikhail Osher <miraage.wow@gmail.com>
2012-08-31 13:59:51 +0400

    Spaces to Tabs

--- framework/web/CHttpRequest.php
+++ framework/web/CHttpRequest.php
@@ -960,30 +960,30 @@ class CHttpRequest extends CApplicationComponent
 	 */
 	public function validateCsrfToken($event)
 	{
-        if ($this->getIsPostRequest() ||
-            $this->getIsPutRequest() ||
-            $this->getIsDeleteRequest())
-        {
-            $cookies = $this->getCookies();
+		if ($this->getIsPostRequest() ||
+			$this->getIsPutRequest() ||
+			$this->getIsDeleteRequest())
+		{
+			$cookies=$this->getCookies();
 
-            $method = $this->getRequestType();
-            if ($method === 'POST')
-                $userToken = $this->getPost($this->csrfTokenName);
-            elseif ($method === 'PUT')
-                $userToken = $this->getPut($this->csrfTokenName);
-            else
-                $userToken = $this->getDelete($this->csrfTokenName);
+			$method = $this->getRequestType();
+			if ($method === 'POST')
+				$userToken=$this->getPost($this->csrfTokenName);
+			elseif ($method === 'PUT')
+				$userToken=$this->getPut($this->csrfTokenName);
+			else
+				$userToken=$this->getDelete($this->csrfTokenName);
 
-            if (!empty($userToken) && $cookies->contains($this->csrfTokenName))
-            {
-                $cookieToken = $cookies->itemAt($this->csrfTokenName)->value;
-                $valid = $cookieToken === $userToken;
-            }
-            else
-                $valid = false;
-            if (!$valid)
-                throw new CHttpException(400,Yii::t('yii','The CSRF token could not be verified.'));
-        }
+			if (!empty($userToken) && $cookies->contains($this->csrfTokenName))
+			{
+				$cookieToken=$cookies->itemAt($this->csrfTokenName)->value;
+				$valid=$cookieToken===$userToken;
+			}
+			else
+				$valid = false;
+			if (!$valid)
+				throw new CHttpException(400,Yii::t('yii','The CSRF token could not be verified.'));
+		}
 	}
 }
 
