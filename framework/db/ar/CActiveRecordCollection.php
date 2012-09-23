7de361f02c84524c5e32aaab16f47605c7206d65
Alexander Kochetov <creocoder@gmail.com>
2012-09-23 17:47:31 +0400

    CActiveRecordCollection::save() fix

--- framework/db/ar/CActiveRecordCollection.php
+++ framework/db/ar/CActiveRecordCollection.php
@@ -1,31 +1,16 @@
 <?php
 class CActiveRecordCollection extends CModelCollection
 {
-	public function insert($attributes=null)
-	{
-		$result=true;
-
-		foreach($this as $model)
-			$result=$model->insert($attributes) && $result;
-
-		return $result;
-	}
-
-	public function update($attributes=null)
-	{
-		$result=true;
-
-		foreach($this as $model)
-			$result=$model->update($attributes) && $result;
-
-		return $result;
-	}
-
 	public function save($runValidation=true,$attributes=null)
 	{
-		if(!$runValidation || $this->validate($attributes))
-			return $this->getIsNewRecord() ? $this->insert($attributes) : $this->update($attributes);
-		else
+		if($runValidation && !$this->validate($attributes))
 			return false;
+
+		$result=true;
+
+		foreach($this as $model)
+			$result=($model->getIsNewRecord() ? $model->insert($attributes) : $model->update($attributes)) && $result;
+
+		return $result;
 	}
 }
