e7999f5aaa052d7e9478e0ff52d96032dfa4d2c7
slavcopost <slavcopost@gmail.com>
2012-09-25 00:46:24 +0300

    CSqlDataProvider support CDbCommand fix

--- framework/web/CSqlDataProvider.php
+++ framework/web/CSqlDataProvider.php
@@ -73,24 +73,35 @@ class CSqlDataProvider extends CDataProvider
 	 */
 	protected function fetchData()
 	{
-		if(!$this->sql instanceof CDbCommand)
-		{
-			$db=$this->db===null ? Yii::app()->db : $this->db;
-			$db->active=true;
-			$command=$db->createCommand($this->sql);
-		}
+		$db=$this->db===null ? Yii::app()->db : $this->db;
+		$db->active=true;
+
+		if($this->sql instanceof CDbCommand)
+			$sql=$this->sql->getText();
 		else
-			$command=$this->sql;
+			$sql=$this->sql;
 
 		if(($sort=$this->getSort())!==false)
-			$command->order($sort->getOrderBy());
+		{
+			$order=$sort->getOrderBy();
+			if(!empty($order))
+			{
+				if(preg_match('/\s+order\s+by\s+[\w\s,]+$/i',$sql))
+					$sql.=', '.$order;
+				else
+					$sql.=' ORDER BY '.$order;
+			}
+		}
 
 		if(($pagination=$this->getPagination())!==false)
 		{
 			$pagination->setItemCount($this->getTotalItemCount());
-			$command->limit($pagination->getLimit(), $pagination->getOffset());
+			$limit=$pagination->getLimit();
+			$offset=$pagination->getOffset();
+			$sql=$db->getCommandBuilder()->applyLimit($sql,$limit,$offset);
 		}
 
+		$command=$db->createCommand($sql);
 		foreach($this->params as $name=>$value)
 			$command->bindValue($name,$value);
 
