5f7f3df796c3e259e170c9fe4dac6029e6589ae0
Sonko Dmitry <st.difor@gmail.com>
2012-10-17 12:24:29 +0600

    Gii model code generator fixed
    If we use "schemaCachingDuration" value in db config, then use Gii model
    generator we have one problem. If table scheme updated without
    migration, we have old schema cache. I'm offer forcibly update db scheme
    if "schemaCachingDuration" used.

--- framework/gii/generators/model/ModelCode.php
+++ framework/gii/generators/model/ModelCode.php
@@ -188,6 +188,8 @@ class ModelCode extends CCodeModel
 
 	public function getTableSchema($tableName)
 	{
+        if (Yii::app()->{$this->connectionId}->schemaCachingDuration!==0)
+            Yii::app()->{$this->connectionId}->schema->refresh();
 		return Yii::app()->{$this->connectionId}->getSchema()->getTable($tableName);
 	}
 
