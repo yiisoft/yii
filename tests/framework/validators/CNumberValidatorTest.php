48d9d6ec9d226dd75bff4369d08b3c684b25f995
resurtm <resurtm@gmail.com>
2012-11-07 23:01:05 +0600

    Forgot about test itself.

---
+++ tests/framework/validators/CNumberValidatorTest.php
@@ -0,0 +1,32 @@
+<?php
+require_once('ValidatorTestModel.php');
+
+class CNumberValidatorTest extends CTestCase
+{
+	public function providerIssue1669()
+	{
+		return array(
+			array('number1', 4, array('number1' => array('Number1 is too small (minimum is 5).'))),
+			array('number1', 5, array()),
+			array('number2', 15, array()),
+			array('number2', 16, array('number2' => array('Number2 is too big (maximum is 15).'))),
+
+			array('number1', 'a4', array('number1' => array('Number1 must be a number.'))),
+			array('number1', 'b5', array('number1' => array('Number1 must be a number.'))),
+			array('number2', 'c15', array('number2' => array('Number2 must be an integer.'))),
+			array('number2', 'd16', array('number2' => array('Number2 must be an integer.'))),
+		);
+	}
+
+	/**
+	 * https://github.com/yiisoft/yii/issues/1669
+	 * @dataProvider providerIssue1669
+	 */
+	public function testIssue1669($attribute, $number, $assertion)
+	{
+		$model = new ValidatorTestModel('CNumberValidatorTest');
+		$model->$attribute = $number;
+		$model->validate(array($attribute));
+		$this->assertEquals($assertion, $model->getErrors());
+	}
+}
