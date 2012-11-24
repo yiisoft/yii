f5231f456a4f2cb881260372abd680b0d41045a9
Alexander Makarov <sam@rmcreative.ru>
2012-11-24 17:15:21 +0400

    Fixes #1750. API doc adjustments.

--- framework/base/interfaces.php
+++ framework/base/interfaces.php
@@ -337,8 +337,8 @@ interface IAuthManager
 {
 	/**
 	 * Performs access check for the specified user.
-	 * @param string $itemName the name of the operation that need access check
-	 * @param mixed $userId the user ID. This should can be either an integer and a string representing
+	 * @param string $itemName the name of the operation that we are checking access to
+	 * @param mixed $userId the user ID. This should be either an integer or a string representing
 	 * the unique identifier of a user. See {@link IWebUser::getId}.
 	 * @param array $params name-value pairs that would be passed to biz rules associated
 	 * with the tasks and roles assigned to the user.
