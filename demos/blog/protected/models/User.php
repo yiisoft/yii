1a607eebdc77c2f9219e1fa6d7697f92b03cda80
tom-- <fsb@thefsb.org>
2012-11-28 10:35:00 -0500

    updated guide and blog tutorial acording to pull request comments

--- demos/blog/protected/models/User.php
+++ demos/blog/protected/models/User.php
@@ -91,6 +91,14 @@ class User extends CActiveRecord
 
 	/**
 	 * Generates a salt that can be used to generate a password hash.
+	 *
+	 * The {@link http://php.net/manual/en/function.crypt.php PHP `crypt()` built-in function}
+	 * requires, for the Blowfish hash algorithm, a salt string in a specific format:
+	 *  - "$2a$"
+	 *  - a two digit cost parameter
+	 *  - "$"
+	 *  - 22 characters from the alphabet "./0-9A-Za-z".
+	 *
 	 * @param int cost parameter for Blowfish hash algorithm
 	 * @return string the salt
 	 */
@@ -99,12 +107,17 @@ class User extends CActiveRecord
 		if(!is_numeric($cost)||$cost<4||$cost>31){
 			throw new CException(Yii::t('Cost parameter must be between 4 and 31.'));
 		}
-		$rand=array();
+		// Get some pseudo-random data from mt_rand().
+		$rand='';
 		for($i=0;$i<8;++$i)
-			$rand[]=pack('S',mt_rand(0,0xffff));
-		$rand[]=substr(microtime(),2,6);
-		$rand=sha1(implode('',$rand),true);
+			$rand.=pack('S',mt_rand(0,0xffff));
+		// Add the microtime for a little more entropy.
+		$rand.=microtime();
+		// Mix the bits cryptographically.
+		$rand=sha1($rand,true);
+		// Form the prefix that specifies hash algorithm type and cost parameter.
 		$salt='$2a$'.str_pad((int)$cost,2,'0',STR_PAD_RIGHT).'$';
+		// Append the random salt string in the required base64 format.
 		$salt.=strtr(substr(base64_encode($rand),0,22),array('+'=>'.'));
 		return $salt;
 	}
