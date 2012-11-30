d24646a813426c99c7c2765d8c3c8bb5dead3fc1
tom-- <fsb@thefsb.org>
2012-11-30 12:01:40 -0500

    Better documentation for CPasswordManager

--- framework/base/CPasswordManager.php
+++ framework/base/CPasswordManager.php
@@ -1,70 +1,78 @@
 <?php
+/**
+ * This file contains classes implementing password manager functions.
+ *
+ * @author Tom Worster <fsb@thefsb.org>
+ */
 
 /**
- * CPasswordManager provides a simple API for secure password hashing and verification,
- * useful, for example, in storing passwords in a database.
- *
- *
- * CPasswordManager can be used as follows:
- *
- * Configure the application component in the application config file:
- * <code>
- *    return array(
- *        ...
- *        'components' => array(
- *            'passwordManager' => array(
- *                'class' => 'CPasswordManager',
- *                'cost' => 14,
- *            ),
- *            ....
- * </code>
- *
- * Get an instance. CPasswordManager will throw an exception here if the Blowfish hash algorithm is not
- * available in PHP's crypt() function:
- * <code>
- *    $pm = Yii::app()->passwordManager;
- * </code>
- *
- * Generate a hash from a password:
- * <code>
- *    $hash = $pw->hashPassword($password);
- * </code>
- * This hash can be stored in a database field such as: CHAR(64) CHARACTER SET latin1. The
- * hash is usually generated and saved to the database when the user provides a new password.
- * But it can also be useful to do so after validating a user's password in order to change
- * the cost or refresh the salt.
- *
- * To verify a password, fetch the user's saved hash from the database (into $hash) and:
- * <code>
- *    if ($pw->verifyPassword($password, $hash)
- *        // password is good
- *    else
- *        // password is bad
- * </code>
+ * CPasswordManager provides a simple API for secure password hashing and verification.
  *
  * CPasswordManager uses the Blowfish hash algorithm available in many PHP runtime
- * environments through the PHP {@link http://php.net/manual/en/function.crypt.php `crypt()`}
- * built-in function. At present (2012) this is the strongest algorithm available in PHP
+ * environments through the PHP {@link http://php.net/manual/en/function.crypt.php crypt()}
+ * built-in function. As of Dec 2012 it is the strongest algorithm available in PHP
  * and the only algorithm without some security concerns surrounding it. For this reason,
  * CPasswordManager fails to initialize when run in and environment that does not have
- * crypt() and its Blowfish option. Systems that have crypt()-Blowfish include:
- *  - Most *nix systems since PHP 4 (the algorithm is part of the library function crypt(3))
- *  - All PHP systems since 5.3.0
- *  - All PHP systems with the {@link http://www.hardened-php.net/suhosin/ Suhosin patch}
+ * crypt() and its Blowfish option. Systems with the option include:
+ * (1) Most *nix systems since PHP 4 (the algorithm is part of the library function crypt(3));
+ * (2) All PHP systems since 5.3.0; (3) All PHP systems with the
+ * {@link http://www.hardened-php.net/suhosin/ Suhosin patch}.
+ * For more information about password hashing, crypt() and Blowfish, please read
+ * the Yii Wiki article
+ * {@link http://www.yiiframework.com/wiki/425/use-crypt-for-password-storage/ Use crypt() for password storage}.
+ * and the
+ * PHP RFC {@link http://wiki.php.net/rfc/password_hash Adding simple password hashing API}.
  *
+ * CPasswordManager can be used as follows. Configure the application component in the application configuration:
+ * <pre>
+ * 'components' => array(
+ *     'passwordManager' => array(
+ *         'class' => 'CPasswordManager',
+ *         'cost' => 14,
+ *     ),
+ *     ....
+ * </pre>
+ * When you first use the CPasswordManager application component, it will throw an
+ * exception if the Blowfish hash algorithm is not
+ * available in PHP's crypt() function.
  *
+ * Generate a hash from a password:
+ * <pre>
+ * $hash = Yii::app()->passwordManager->hashPassword($password);
+ * </pre>
+ * This hash can be stored in a database (e.g. CHAR(64) CHARACTER SET latin1). The
+ * hash is usually generated and saved to the database when the user enters a new password.
+ * But it can also be useful to generate and save a hash after validating a user's
+ * password in order to change the cost or refresh the salt.
  *
- * @property int $cost Cost parameter of the Blowfish hash algorithm. The higher the cost,
- * the longer it takes to generate a hash and  to verify a password, consequently it also
- * slows down a brute-force attack. For best protection, set it to the highest value that
- * is tolerable on production servers.
+ * To verify a password, fetch the user's saved hash from the database (into $hash) and:
+ * <pre>
+ * if (Yii::app()->passwordManager->verifyPassword($password, $hash)
+ *     // password is good
+ * else
+ *     // password is bad
+ * </pre>
+ *
+ * @property int $cost Cost parameter of the Blowfish hash algorithm.
+ *
+ * @author Tom Worster <fsb@thefsb.org>
+ * @package system.base
+ * @since 1.1.14
  */
 class CPasswordManager extends CApplicationComponent implements IApplicationComponent
 {
+    /**
+     * @var int Cost parameter used by the Blowfish hash algorithm.
+     * The higher the cost,
+     * the longer it takes to generate a hash and to verify a password, consequently it also
+     * slows down a brute-force attack. For best protection, set it to the highest value that
+     * is tolerable on production servers.
+     */
     protected  $cost = 12;
 
     /**
-     * @throws CException if the runtime system does not have required features.
+     * Initialize the CPasswordManager application component.
+     * @throws CException if the runtime system does not have PHP crypt() with the Blowfish hash option.
      */
     public function init()
     {
@@ -85,6 +93,11 @@ class CPasswordManager extends CApplicationComponent implements IApplicationComp
         parent::init();
     }
 
+    /**
+     * Set the cost parameter used by the Blowfish hash algorithm.
+     * @param int $cost cost parameter to use when hashing passwords.
+     * @throws CException if cost parameters is invalid.
+     */
     public function setCost($cost)
     {
         if (!is_numeric($cost)) {
@@ -103,6 +116,10 @@ class CPasswordManager extends CApplicationComponent implements IApplicationComp
         $this->cost = $cost;
     }
 
+    /**
+     * Return the cost parameter used by the Blowfish hash algorithm.
+     * @return int cost parameter used when hashing passwords.
+     */
     public function getCost()
     {
         return $this->cost;
@@ -110,7 +127,7 @@ class CPasswordManager extends CApplicationComponent implements IApplicationComp
 
     /**
      * Generate a secure hash from a password and a random salt using the
-     * PHP {@link http://php.net/manual/en/function.crypt.php `crypt()`} built-in function
+     * PHP {@link http://php.net/manual/en/function.crypt.php crypt()} built-in function
      * with the Blowfish hash option.
      *
      * @param string $password The password to be hashed.
@@ -153,12 +170,12 @@ class CPasswordManager extends CApplicationComponent implements IApplicationComp
     /**
      * Generates a salt that can be used to generate a password hash.
      *
-     * The PHP {@link http://php.net/manual/en/function.crypt.php `crypt()`} built-in function
+     * The PHP {@link http://php.net/manual/en/function.crypt.php crypt()} built-in function
      * requires, for the Blowfish hash algorithm, a salt string in a specific format:
-     *  - "$2a$", in which the "a" may be replaced by "x" or "y", see PHP manual for details
-     *  - a two digit cost parameter
-     *  - "$"
-     *  - 22 characters from the alphabet "./0-9A-Za-z".
+     *  "$2a$" (in which the "a" may be replaced by "x" or "y" see PHP manual for details),
+     *  a two digit cost parameter,
+     *  "$",
+     *  22 characters from the alphabet "./0-9A-Za-z".
      *
      * @return string the salt
      */
