26786ccbd60e603c3fe114fecab3a12ea97bb1a3
DaSourcerer <webmaster@dasourcerer.net>
2012-12-26 11:41:40 +0100

    memcache for PHP v5.5

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo 'y' | pecl install memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
+if [[ $TRAVIS_PHP_VERSION < 5.5 ]]; then
+  echo 'y' | pecl install memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
+else
+  wget http://pecl.php.net/get/memcache-2.2.7.tgz
+  tar -zxf memcache-2.2.7.tgz
+  sh -c "cd memcache-2.2.7 && phpize && ./configue --enable-memcache && make && sudo make install"
+  echo "memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+fi
