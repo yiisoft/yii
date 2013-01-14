84bb237d509fee132369efab7813ba6fff393705
njasm <njmorais@proactive-hosters.com>
2013-01-14 19:03:43 +0000

    Added missing activeSearchField() method to CHtml. And searchField() to CActiveForm. For creation of HTML input field of type search, that fixes #1743

--- framework/web/helpers/CHtml.php
+++ framework/web/helpers/CHtml.php
@@ -1258,6 +1258,25 @@ EOD;
 	}
 
 	/**
+	 * Generates a search field input for a model attribute.
+	 * If the attribute has input error, the input field's CSS class will
+	 * be appended with {@link errorCss}.
+	 * @param CModel $model the data model
+	 * @param string $attribute the attribute
+	 * @param array $htmlOptions additional HTML attributes. Besides normal HTML attributes, a few special
+	 * attributes are also recognized (see {@link clientChange} and {@link tag} for more details.)
+	 * @return string the generated input field
+	 * @see clientChange
+	 * @see activeInputField
+	 */
+	public static function activeSearchField($model,$attribute,$htmlOptions=array())
+	{
+		self::resolveNameID($model,$attribute,$htmlOptions);
+		self::clientChange('change',$htmlOptions);
+		return self::activeInputField('search',$model,$attribute,$htmlOptions);
+	}
+        
+        /**
 	 * Generates a url field input for a model attribute.
 	 * If the attribute has input error, the input field's CSS class will
 	 * be appended with {@link errorCss}.
