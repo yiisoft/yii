3c339515d5ccb565c9389e4802ffbe4716902d35
Kamil Dziedzic <arvenil@klecza.pl>
2013-02-18 23:10:12 +0100

    bugfix: memcache extension wasn't loaded

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -6,5 +6,5 @@ else
   wget http://pecl.php.net/get/memcache-2.2.7.tgz
   tar -zxf memcache-2.2.7.tgz
   sh -c "cd memcache-2.2.7 && phpize && ./configure --enable-memcache && make && sudo make install"
-  echo "memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+  echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
 fi
