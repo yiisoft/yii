7544da0a3a5eb9197dfedab864b4a434c775f4b9
DaSourcerer <webmaster@dasourcerer.net>
2013-03-11 16:32:17 +0100

    New go at registering the memcache extension the currently recommended way

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 install_memcache() {
-    if [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
-        echo 'y' | pecl install memcache
-    else
+    if [ "$(expr "$TRAVIS_PHP_VERSION" ">=" "5.5")" -eq 1 ]; then
         MEMCACHE_VERSION="2.2.7"
         wget "http://pecl.php.net/get/memcache-$MEMCACHE_VERSION.tgz" &&
         tar -zxf "memcache-$MEMCACHE_VERSION.tgz" &&
         sh -c "cd memcache-$MEMCACHE_VERSION && phpize && ./configure --enable-memcache && make && sudo make install" &&
-        echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
     fi
+
+    echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
     return $?
 }
