a349500187cf71ea151dbd3f8e7775ba5bd4c98b
DaSourcerer <webmaster@dasourcerer.net>
2013-03-11 16:29:01 +0100

    Revert to 9a216ae3bb3a29ebd28ce8299897c72e74a75473

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-echo "extension=memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+install_memcache() {
+    if [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
+        echo 'y' | pecl install memcache
+    else
+        MEMCACHE_VERSION="2.2.7"
+        wget "http://pecl.php.net/get/memcache-$MEMCACHE_VERSION.tgz" &&
+        tar -zxf "memcache-$MEMCACHE_VERSION.tgz" &&
+        sh -c "cd memcache-$MEMCACHE_VERSION && phpize && ./configure --enable-memcache && make && sudo make install" &&
+        echo "extension=memcache.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s/.*:\s*//"`
+    fi
+
+    return $?
+}
+
+install_memcache > ~/memcache.log || ( echo "=== MEMCACHE BUILD FAILED ==="; cat ~/memcache.log )
