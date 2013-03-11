a48881dc115c2d071b091e1f4b49f4fabd41d6ce
DaSourcerer <webmaster@dasourcerer.net>
2013-03-11 16:11:34 +0100

    s/memcache/memcached/

--- tests/travis/memcache-setup.sh
+++ tests/travis/memcache-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension=memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
