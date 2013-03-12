307a190a8328607655f548fc43062d2796447e1f
CeBe <mail@cebe.cc>
2013-03-12 17:11:50 +0100

    some adjustments to pull request #2159

--- framework/db/schema/mysql/CMysqlCommandBuilder.php
+++ framework/db/schema/mysql/CMysqlCommandBuilder.php
@@ -20,6 +20,7 @@ class CMysqlCommandBuilder extends CDbCommandBuilder
 	/**
 	 * Alters the SQL to apply JOIN clause.
 	 * This method handles the mysql specific syntax where JOIN has to come before SET in UPDATE statement
+	 * and for DELETE where JOIN has to be after FROM part.
 	 * @param string $sql the SQL statement to be altered
 	 * @param string $join the JOIN clause (starting with join type, such as INNER JOIN)
 	 * @return string the altered SQL statement
@@ -31,9 +32,9 @@ class CMysqlCommandBuilder extends CDbCommandBuilder
 
 		if(strpos($sql,'UPDATE')===0 && ($pos=strpos($sql,'SET'))!==false)
 			return substr($sql,0,$pos).$join.' '.substr($sql,$pos);
-		elseif(strpos($sql,'DELETE')===0 && $join)
+		elseif(strpos($sql,'DELETE FROM ')===0)
 		{
-			$tableName = substr($sql, strlen('DELETE FROM '));
+			$tableName=substr($sql,12);
 			return "DELETE {$tableName} FROM {$tableName} ".$join;
 		}
 		else
