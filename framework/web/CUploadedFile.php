ccfff850acba0789cb7f1483c20028824f6d11e1
ch3nz <fantcbug@gmail.com>
2013-03-14 16:16:58 +0800

    fixed:save as other file

--- framework/web/CUploadedFile.php
+++ framework/web/CUploadedFile.php
@@ -192,6 +192,7 @@ class CUploadedFile extends CComponent
 	{
 		if($this->_error==UPLOAD_ERR_OK)
 		{
+			$this->_name = basename($file);
 			if($deleteTempFile)
 				return move_uploaded_file($this->_tempName,$file);
 			elseif(is_uploaded_file($this->_tempName))
