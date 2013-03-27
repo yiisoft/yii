a8cef857b5a9318631a914ed3e74673854a76dc1
Gustavo Salomé Silva <gustavonips@gmail.com>
2013-03-27 19:58:46 -0300

    Added test for CModel::addValidator method

--- tests/framework/base/CModelTest.php
+++ tests/framework/base/CModelTest.php
@@ -239,4 +239,14 @@ class CModelTest extends CTestCase
 				$e->getMessage());
 		}
 	}
+	public function testAddValidator()
+	{
+	    $model = new NewModel('test');
+	    $model->addValidator('required', 'firstName', array('on' => 'test'));
+	    $this->assertTrue( $model->isAttributeRequired('firstName') );
+	    $validators = $this->getValidatorList();
+	    //gets last validator in the list
+	    $validator = $validators->itemAt( $validators->count() - 1 );
+	    $this->assertTrue( $validator instanceof CRequiredValidator );
+	}
 }
