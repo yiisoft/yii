d435cd8b43313717b4dec2e3c2c4f0f0458d08cd
Klimov Paul <klimov.paul@gmail.com>
2013-03-29 21:09:00 +0200

    Unit test "CDbCommandEngineVaryTest" has been created.

---
+++ tests/framework/db/CDbCommandEngineVaryTest.php
@@ -0,0 +1,121 @@
+<?php
+
+Yii::import('system.db.CDbConnection');
+
+/**
+ * Test case for "system.db.CDbCommand".
+ * This test case checks functions and features, which implementation varies
+ * from one database engine to another.
+ *
+ * @see CDbCommand
+ */
+class CDbCommandEngineVaryTest extends CTestCase
+{
+	/**
+	 * @var array test db connections
+	 */
+	protected $dbConnections=array();
+
+	public function setUp()
+	{
+		if(!extension_loaded('pdo'))
+			$this->markTestSkipped('PDO extension is required.');
+	}
+
+	public function tearDown()
+	{
+		if(!empty($this->dbConnections))
+			foreach($this->dbConnections as $dbConnection)
+				if(is_object($dbConnection))
+					$dbConnection->active=false;
+	}
+
+	/**
+	 * @return array list of test db connections.
+	 */
+	protected function getTestDbConnections()
+	{
+		if(empty($this->dbConnections))
+			$this->dbConnections=$this->generateTestDbConnections();
+		return $this->dbConnections;
+	}
+
+	/**
+	 * Generates list of test db connections in format driverName => CDbConnection instance
+	 * If connection for specific driver can not be established its key will contain "false".
+	 * @return array list of test db connections.
+	 */
+	protected function generateTestDbConnections()
+	{
+		$dbDrivers=array(
+			'sqlite',
+			'mysql',
+		);
+		$dbConnections=array();
+		foreach($dbDrivers as $dbDriver)
+		{
+			if(!extension_loaded('pdo_mysql'))
+				$dbConnections[$dbDriver]=false;
+
+			if($dbDriver=='sqlite')
+				$dbConnection=new CDbConnection('sqlite::memory:');
+			else
+				$dbConnection=new CDbConnection($dbDriver.':host=127.0.0.1;dbname=yii','test','test');
+			try
+			{
+				$dbConnection->active=true;
+				$dbConnection->pdoInstance->exec(file_get_contents(dirname(__FILE__)."/data/{$dbDriver}.sql"));
+				$dbConnections[$dbDriver]=$dbConnection;
+			}
+			catch(Exception $e)
+			{
+				$dbConnections[$dbDriver]=false;
+			}
+		}
+		return $dbConnections;
+	}
+
+	/**
+	 * Data provider for test database connections.
+	 * @return array list of data in format array($driverName,$dbConnection)
+	 */
+	public function dataProviderDbConnections()
+	{
+		$data=array();
+		$dbConnections=$this->getTestDbConnections();
+		foreach($dbConnections as $driverName=>$dbConnection)
+			$data[]=array($driverName,$dbConnection);
+		return $data;
+	}
+
+	// Tests:
+
+	/**
+	 * @dataProvider dataProviderDbConnections
+	 *
+	 * @param string $driverName
+	 * @param CDbConnection|false $dbConnection
+	 */
+	public function testInsertMultiple($driverName,$dbConnection)
+	{
+		if (!is_object($dbConnection))
+			$this->markTestSkipped("Failed to connect '{$driverName}' test database.'");
+
+		$multipleInsertCommand=$dbConnection->createCommand();
+
+		$tableName='types';
+		$columns=array(
+			'int_col',
+			'char_col',
+		);
+		$values=array(
+			array(1,'char_1'),
+			array(2,'char_2'),
+		);
+		$multipleInsertCommand->insertMultiple($tableName,$columns,$values);
+
+		$rows=$dbConnection->createCommand('SELECT * FROM '.$dbConnection->quoteTableName($tableName))->query();
+
+		$this->assertEquals(count($values),count($rows));
+	}
+}
