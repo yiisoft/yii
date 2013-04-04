1c927806d67badf338ba79b33a78f2f3c29f5227
marcovtwout <mr@vtwout.net>
2013-04-04 15:02:20 +0200

    Add Unit test for reusableData test, see #2290

--- tests/framework/caching/CCacheDependencyTest.php
+++ tests/framework/caching/CCacheDependencyTest.php
@@ -34,8 +34,48 @@ class CCacheDependencyTest extends CTestCase
 		$dependency3 = new MockDirectoryCacheDependency(dirname($tempFile));
 		$dependency3->reuseDependentData = true;
 		$dependency3->evaluateDependency();
-		$this->assertTrue($dependency3->getHasChanged());
+		// Behavior is slightly modified after patching CCacheDependency, but in my opinion, correctly.
+		//
+		// Before: after calling evaluateDependency(), hasChanged() would keep returning 'true',
+		// as $_reusableData' also stored the result of the comparison.
+		//
+		// Now: since the dependency data is always compared to the stored value in $_reusableData, after calling evaluateDependency(),
+		// hasChanged() will return 'false'.
+		//$this->assertTrue($dependency3->getHasChanged());
+		$this->assertFalse($dependency3->getHasChanged());
 		$this->assertEquals(1,MockDirectoryCacheDependency::$generateDependentDataCalled);
+	}
+
+	/**
+	 * @note This test requires CCacheDependency::$_reusableData to be made public.
+	 */
+	public function testReusableWithStoredDependencies()
+	{
+		$stateName = 'test';
+		$storedValue = 500;
+		Yii::app()->setGlobalState($stateName, $storedValue);
+		$dependency = new CGlobalStateCacheDependency($stateName);
+		$dependency->reuseDependentData = true;
+
+		$fileCache = new CFileCache;
+		$fileCache->set('foo', $storedValue, 0, $dependency);
+		$fileCache->set('bar', $storedValue, 0, $dependency);
+
+		$this->assertTrue($fileCache->get('foo')==$storedValue);
+
+		/** NEW REQUEST **/
+		CGlobalStateCacheDependency::$_reusableData=array();	// reset $_reusableData
+
+		Yii::app()->setGlobalState($stateName, $storedValue+1);	// changing state should invalidate cache
+
+		$this->assertFalse($fileCache->get('foo'));
+		$fileCache->set('foo', $storedValue+1, 0, $dependency);	// update cache with new value
+
+		/** NEW REQUEST **/
+		CGlobalStateCacheDependency::$_reusableData=array();	// reset $_reusableData
+
+		$this->assertTrue($fileCache->get('foo')==$storedValue+1);
+		$this->assertFalse($fileCache->get('bar'));				// this should be invalidated
 	}
 }
 
