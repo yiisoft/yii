c9d2e4f5c84c5c0e5ae342223d3f8d3efc926107
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2013-04-17 00:01:25 +0600

    Added CRequiredValidator test

--- tests/framework/validators/ValidatorTestModel.php
+++ tests/framework/validators/ValidatorTestModel.php
@@ -3,12 +3,14 @@ class ValidatorTestModel extends CFormModel
 {
 	public $email;
 	public $url;
+	public $username;
 
 	public function rules()
 	{
 		return array(
 			array('email', 'email', 'allowEmpty' => false, 'on' => 'CEmailValidatorTest'),
 			array('url', 'url', 'allowEmpty' => false, 'on' => 'CUrlValidatorTest'),
+			array('username', 'required', 'trim' => false, 'on' => 'CRequiredValidatorTest'),
 		);
 	}
 }
