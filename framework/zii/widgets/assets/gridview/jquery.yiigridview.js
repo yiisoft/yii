eee8cd032c64fe82a83866ee8f0c26c7f440f34b
Gustavo Salomé Silva <gustavonips@gmail.com>
2013-04-28 04:58:07 -0300

    CGridView: Cancel current ajax request before create another
    If you create another ajax request, either by changing the sort or click on a pagination link, it will currently just add another ajax request to the stack.
    A better way would be to cancel the last request and then make a new one.
    This is a problem when the response time is high.

--- framework/zii/widgets/assets/gridview/jquery.yiigridview.js
+++ framework/zii/widgets/assets/gridview/jquery.yiigridview.js
@@ -8,7 +8,7 @@
  */
 
 (function ($) {
-	var selectCheckedRows, methods,
+	var selectCheckedRows, methods, yiiXHR,
 		gridSettings = [];
 	/**
 	 * 1. Selects rows that have checkbox checked (only checkbox that is connected with selecting a row)
@@ -245,13 +245,13 @@
 					$grid = $(this),
 					id = $grid.attr('id'),
 					settings = gridSettings[id];
-				$grid.addClass(settings.loadingClass);
 
 				options = $.extend({
 					type: settings.ajaxType,
 					url: $grid.yiiGridView('getUrl'),
 					success: function (data) {
 						var $data = $('<div>' + data + '</div>');
+						yiiXHR = null;
 						$grid.removeClass(settings.loadingClass);
 						$.each(settings.ajaxUpdate, function (i, el) {
 							var updateId = '#' + el;
@@ -266,6 +266,7 @@
 					},
 					error: function (XHR, textStatus, errorThrown) {
 						var ret, err;
+						yiiXHR = null;
 						$grid.removeClass(settings.loadingClass);
 						if (XHR.readyState === 0 || XHR.status === 0) {
 							return;
@@ -312,13 +313,18 @@
 						options.data = $(settings.filterSelector).serialize();
 					}
 				}
-
+				if(yiiXHR != null){//there is an ajax request, cancel it
+					yiiXHR.abort();
+				}
+				//class must be add after yiiXHR.abort otherwise ajax.error will remove it
+				$grid.addClass(settings.loadingClass);
+				
 				if (settings.ajaxUpdate !== false) {
 					options.url = $.param.querystring(options.url, settings.ajaxVar + '=' + id);
 					if (settings.beforeAjaxUpdate !== undefined) {
 						settings.beforeAjaxUpdate(id, options);
 					}
-					$.ajax(options);
+					yiiXHR = $.ajax(options);
 				} else {  // non-ajax mode
 					if (options.type === 'GET') {
 						window.location.href = options.url;
