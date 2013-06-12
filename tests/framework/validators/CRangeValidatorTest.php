e0a45a0ffae295fe4ff221aaf9aa8b88219727a8
Alexander Makarov <sam@rmcreative.ru>
2013-06-12 14:33:16 +0400

    more tests

--- tests/framework/validators/CRangeValidatorTest.php
+++ tests/framework/validators/CRangeValidatorTest.php
@@ -6,24 +6,30 @@ class CRangeValidatorTest extends CTestCase
 	public function testEmpty()
 	{
 		$model = new ValidatorTestModel('CRangeValidatorTest');
-		$model->number = '';
-		$model->validate(array('number'));
-		$this->assertArrayHasKey('number', $model->getErrors());
+		$model->string1 = '';
+		$model->string2 = '';
+		$model->validate(array('string1','string2'));
+		$this->assertArrayHasKey('string1', $model->getErrors());
+		$this->assertArrayNotHasKey('string2', $model->getErrors());
 	}
 
 	public function testZeroString()
 	{
 		$model = new ValidatorTestModel('CRangeValidatorTest');
-		$model->number = '0';
-		$model->validate(array('number'));
-		$this->assertArrayNotHasKey('number', $model->getErrors());
+		$model->string1 = '0';
+		$model->string2 = '0';
+		$model->validate(array('string1','string2'));
+		$this->assertArrayNotHasKey('string1', $model->getErrors());
+		$this->assertArrayHasKey('string1', $model->getErrors());
 	}
 
 	public function testZeroNumber()
 	{
 		$model = new ValidatorTestModel('CRangeValidatorTest');
-		$model->number = 0;
-		$model->validate(array('number'));
-		$this->assertArrayNotHasKey('number', $model->getErrors());
+		$model->string1 = 0;
+		$model->string2 = 0;
+		$model->validate(array('string1','string2'));
+		$this->assertArrayNotHasKey('string1', $model->getErrors());
+		$this->assertArrayHasKey('string2', $model->getErrors());
 	}
 }
