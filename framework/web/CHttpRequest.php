760c95862f19321ccc8ae728ed6669f6b66a7808
Jani Mikkonen <jani@mikkonen.info>
2013-07-22 12:37:30 +0300

    Update getUserHostAddress()

--- framework/web/CHttpRequest.php
+++ framework/web/CHttpRequest.php
@@ -666,20 +666,15 @@ class CHttpRequest extends CApplicationComponent
 	 */
 	public function getUserHostAddress()
 	{
-		foreach(array('HTTP_CLIENT_IP','HTTP_X_FORWARDED_FOR','HTTP_X_FORWARDED','HTTP_X_CLUSTER_CLIENT_IP','HTTP_FORWARDED_FOR','HTTP_FORWARDED','REMOTE_ADDR') as $key)
+		$addr=isset($_SERVER['REMOTE_ADDR'])?$_SERVER['REMOTE_ADDR']:'127.0.0.1';
+		foreach(explode(',',$addr) as $ip)
 		{
-			if(array_key_exists($key,$_SERVER)===true)
-			{
-				foreach(explode(',',$_SERVER[$key]) as $ip)
-				{
-					$ip=trim($ip);
-					if(version_compare(PHP_VERSION,'5.2.0')>=0)
-						if(filter_var($ip,FILTER_VALIDATE_IP,FILTER_FLAG_NO_PRIV_RANGE|FILTER_FLAG_NO_RES_RANGE)!==false)
-							return $ip;
-					else
-						return $ip;
-				}
-			}
+			$ip=trim($ip);
+			if(version_compare(PHP_VERSION,'5.2.0')>=0)
+				if(filter_var($ip,FILTER_VALIDATE_IP,FILTER_FLAG_NO_PRIV_RANGE|FILTER_FLAG_NO_RES_RANGE)!==false)
+					return $ip;
+			else
+				return $ip;
 		}
 		return '127.0.0.1';
 	}
