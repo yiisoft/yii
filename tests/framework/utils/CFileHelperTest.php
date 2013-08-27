b1882e8d0254f37fbf002bb28d1133ed18f170cc
Pavel Voronin <pavel.a.voronin@gmail.com>
2013-08-27 12:29:06 +0400

    CFileHelperTest.testFindFiles_absolutePaths was added

--- tests/framework/utils/CFileHelperTest.php
+++ tests/framework/utils/CFileHelperTest.php
@@ -119,6 +119,19 @@ class CFileHelperTest extends CTestCase
 		$this->assertFalse(is_file($bd.$this->rootDir1.$ds.$this->subDir.$ds.$this->file4));
 	}
 
+	public function testFindFiles_absolutePaths()
+	{
+		$this->createTestStruct($this->testDir);
+
+		$bd=$this->testDir.DIRECTORY_SEPARATOR.$this->rootDir1.DIRECTORY_SEPARATOR;
+
+		$files=CFileHelper::findFiles($this->testDir);
+
+		$this->assertEquals($bd.'sub'.DIRECTORY_SEPARATOR.'..svn',$files[0]);
+		$this->assertEquals($bd.'sub'.DIRECTORY_SEPARATOR.'.htaccess',$files[1]);
+		$this->assertEquals($bd.'sub'.DIRECTORY_SEPARATOR.'testfile',$files[2]);
+	}
+
 	private function createTestStruct($testDir)
 	{
 		$rootDir=$testDir.DIRECTORY_SEPARATOR.$this->rootDir1;
