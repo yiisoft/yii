748a86911daece2758d30def69db199737b9737a
Evan King <evan.king@bluespurs.com>
2013-11-05 14:10:42 -0400

    more whitespace revert

--- framework/gii/generators/model/ModelCode.php
+++ framework/gii/generators/model/ModelCode.php
@@ -392,7 +392,7 @@ class ModelCode extends CCodeModel
 			return $this->modelClass;
 
 		$tableName=$this->removePrefix($tableName,false);
-		if(($pos=strpos($tableName,'.'))!==false)  // remove schema part (e.g. remove 'public2.' from 'public2.post')
+		if(($pos=strpos($tableName,'.'))!==false) // remove schema part (e.g. remove 'public2.' from 'public2.post')
 			$tableName=substr($tableName,$pos+1);
 		$className='';
 		foreach(explode('_',$tableName) as $name)
