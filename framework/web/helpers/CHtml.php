445345a9b5ac6749f4e3588e1d9f4d26416d5daa
Abbas Ali Hashemian <info@namedin.com>
2013-12-12 19:50:14 +0330

    It's better to prevent password field viewstate
    Mostly password fields don't have viewstate in ASP.Net or web frameworks. Because either in HTTP or HTTPS postback communications the posted password will be visible in the source code of the page has been reponded back to the client.
    Affects in the CActiveForm and the ::activePasswordField.

--- framework/web/helpers/CHtml.php
+++ framework/web/helpers/CHtml.php
@@ -2422,7 +2422,7 @@ EOD;
 
 		if($type==='file')
 			unset($htmlOptions['value']);
-		elseif(!isset($htmlOptions['value']))
+		elseif(!isset($htmlOptions['value']) && $type!=='password')
 			$htmlOptions['value']=self::resolveValue($model,$attribute);
 		if($model->hasErrors($attribute))
 			self::addErrorCss($htmlOptions);
