32bc867bf66104bfcef77e4c2222977454352d8f
munawer-t <munawer@xminds.in>
2014-01-07 15:23:17 +0530

    Doc typos and corrections

--- framework/db/ar/CActiveFinder.php
+++ framework/db/ar/CActiveFinder.php
@@ -1503,11 +1503,12 @@ class CStatElement
 			$record->addRelatedRecord($relation->name,isset($stats[$pk])?$stats[$pk]:$relation->defaultValue,false);
 	}
 
-	/*
-	 * @param string $joinTableName jointablename
-	 * @param string $keys keys
-	 */
-	private function queryManyMany($joinTableName,$keys)
+    /**
+     * @param $joinTableName jointablename
+     * @param $keys keys
+     * @throws CDbException
+     */
+    private function queryManyMany($joinTableName,$keys)
 	{
 		$relation=$this->relation;
 		$model=$this->_finder->getModel($relation->className);
