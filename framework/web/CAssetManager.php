b06792d4e911e5f730e195fe7c14e83620e32c89
Carsten Brandt <mail@cebe.cc>
2014-01-28 14:27:48 +0100

    fixed possible problem with realpath and false value
    realpath(false) = current working directory.
    This can cause problems with getAlias() which returns false.
    
    fixes #3113

--- framework/web/CAssetManager.php
+++ framework/web/CAssetManager.php
@@ -211,7 +211,7 @@ class CAssetManager extends CApplicationComponent
 			throw new CException(Yii::t('yii','The "forceCopy" and "linkAssets" cannot be both true.'));
 		if(isset($this->_published[$path]))
 			return $this->_published[$path];
-		elseif(($src=realpath($path))!==false)
+		elseif(is_string($path) && ($src=realpath($path))!==false)
 		{
 			$dir=$this->generatePath($src,$hashByName);
 			$dstDir=$this->getBasePath().DIRECTORY_SEPARATOR.$dir;
@@ -271,7 +271,7 @@ class CAssetManager extends CApplicationComponent
 	 */
 	public function getPublishedPath($path,$hashByName=false)
 	{
-		if(($path=realpath($path))!==false)
+		if(is_string($path) && ($path=realpath($path))!==false)
 		{
 			$base=$this->getBasePath().DIRECTORY_SEPARATOR.$this->generatePath($path,$hashByName);
 			return is_file($path) ? $base.DIRECTORY_SEPARATOR.basename($path) : $base ;
@@ -295,7 +295,7 @@ class CAssetManager extends CApplicationComponent
 	{
 		if(isset($this->_published[$path]))
 			return $this->_published[$path];
-		if(($path=realpath($path))!==false)
+		if(is_string($path) && ($path=realpath($path))!==false)
 		{
 			$base=$this->getBaseUrl().'/'.$this->generatePath($path,$hashByName);
 			return is_file($path) ? $base.'/'.basename($path) : $base;
