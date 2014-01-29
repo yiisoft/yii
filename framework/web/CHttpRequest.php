9a1eac10c4b4912c75088f84f23f26a2c0b93309
Carsten Brandt <mail@cebe.cc>
2014-01-29 01:40:05 +0100

    reverted pull request #2671 for issue #2670

--- framework/web/CHttpRequest.php
+++ framework/web/CHttpRequest.php
@@ -720,13 +720,7 @@ class CHttpRequest extends CApplicationComponent
 	 */
 	public function getUserHostAddress()
 	{
-		$ip=isset($_SERVER['REMOTE_ADDR'])?$_SERVER['REMOTE_ADDR']:'127.0.0.1';
-		if(version_compare(PHP_VERSION,'5.2.0')>=0)
-			if(filter_var($ip,FILTER_VALIDATE_IP,FILTER_FLAG_NO_PRIV_RANGE|FILTER_FLAG_NO_RES_RANGE)!==false)
-				return $ip;
-		else
-			return $ip;
-		return '127.0.0.1';
+		return isset($_SERVER['REMOTE_ADDR'])?$_SERVER['REMOTE_ADDR']:'127.0.0.1';
 	}
 
 	/**
