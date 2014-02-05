474e29f27aaee22b851f7cc9c0617073d5e0dcbd
Alexander Makarov <sam@rmcreative.ru>
2014-02-05 21:29:17 +0400

    And another tial to achieve 5.2 compatibility

--- framework/base/CApplication.php
+++ framework/base/CApplication.php
@@ -413,8 +413,10 @@ abstract class CApplication extends CModule
 	 */
 	public function getLocaleDataPath()
 	{
-		$class=$this->localeClass;
-		return $class::$$dataPath===null ? Yii::getPathOfAlias('system.i18n.data') : $class::$$dataPath;
+		$vars=get_class_vars($this->localeClass);
+		if(empty($vars['dataPath']))
+			return Yii::getPathOfAlias('system.i18n.data');
+		return $vars['dataPath'];
 	}
 
 	/**
@@ -424,8 +426,8 @@ abstract class CApplication extends CModule
 	 */
 	public function setLocaleDataPath($value)
 	{
-		$class=$this->localeClass;
-		$class::$$dataPath=$value;
+		$property=new ReflectionProperty($this->localeClass,'dataPath');
+		$property->setValue($value);
 	}
 
 	/**
