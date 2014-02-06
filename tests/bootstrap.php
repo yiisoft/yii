3739ba8696a51b4817a9bb26a88c1ff7fc1a0925
Joel Marcey <joel@fb.com>
2014-02-06 14:16:49 -0800

    Code guideline changes and changelog addition

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,13 +9,10 @@ $_SERVER['SCRIPT_FILENAME']=__FILE__;
 require_once(dirname(__FILE__).'/../framework/yii.php');
 require_once(dirname(__FILE__).'/TestApplication.php');
 // Support PHPUnit <=3.7 and >=3.8
-if (file_exists('PHPUnit/Framework/TestCase.php')) 
-{
-        require_once('PHPUnit/Framework/TestCase.php'); // <= 3.7
-} else 
-{
-        require_once('src/Framework/TestCase.php'); // >= 3.8
-}
+if (file_exists('PHPUnit/Framework/TestCase.php'))
+  require_once('PHPUnit/Framework/TestCase.php'); // <= 3.7
+else
+  require_once('src/Framework/TestCase.php'); // >= 3.8
 // make sure non existing PHPUnit classes do not break with Yii autoloader
 Yii::$enableIncludePath = false;
 Yii::setPathOfAlias('tests', dirname(__FILE__));
