7ff1374e055f959518d8d9200f0c764721952f87
Carsten Brandt <mail@cebe.cc>
2014-06-03 15:45:42 +0200

    Update CClientScript.php
    code style fixes

--- framework/web/CClientScript.php
+++ framework/web/CClientScript.php
@@ -595,13 +595,10 @@ class CClientScript extends CApplicationComponent
 			$this->recordCachingAction('clientScript','registerCoreScript',$params);
 		}
 		elseif(YII_DEBUG)
-		{
-			throw new CException('There is no ClientScript package: '.$name);
-		}
+			throw new CException('There is no CClientScript package: '.$name);
 		else
-		{
-			Yii::log('There is no ClientScript package: '.$name, CLogger::LEVEL_WARNING, 'system.web.CClientScript');
-		}
+			Yii::log('There is no CClientScript package: '.$name,CLogger::LEVEL_WARNING,'system.web.CClientScript');
+
 		return $this;
 	}
 
