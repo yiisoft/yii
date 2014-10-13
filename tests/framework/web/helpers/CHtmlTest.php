33efd3e95f2d4fa9c1379f47a9f612853a5a0090
Ger Hobbelt <ger@hobbelt.com>
2014-10-13 12:46:56 +0200

    fix #3584: CHtmlTestModel: incorrect constructor

--- tests/framework/web/helpers/CHtmlTest.php
+++ tests/framework/web/helpers/CHtmlTest.php
@@ -1089,7 +1089,7 @@ class CHtmlTestModel extends CModel
 	 */
 	public $attr4;
 
-	public function __constructor(array $properties)
+	public function __construct(array $properties)
 	{
 		foreach($properties as $property=>$value)
 		{
