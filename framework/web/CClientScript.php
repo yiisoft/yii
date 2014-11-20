1e5b6d72c7f998ac68bb840c0fb5dc3b9be86cf2
GS_Prod. <serhioli@mail.ru>
2014-11-20 04:35:18 +0300

    Added variable and validating for loading from CDN

--- framework/web/CClientScript.php
+++ framework/web/CClientScript.php
@@ -350,12 +350,18 @@ class CClientScript extends CApplicationComponent
 			if(!empty($package['js']))
 			{
 				foreach($package['js'] as $js)
-					$jsFiles[$baseUrl.'/'.$js]=$baseUrl.'/'.$js;
+				{
+					$path = preg_match('/^https?:\/\//i', $js) ? $js : $baseUrl . '/' . $js;
+                    			$jsFiles[$path] = $path;
+				}
 			}
 			if(!empty($package['css']))
 			{
 				foreach($package['css'] as $css)
-					$cssFiles[$baseUrl.'/'.$css]='';
+				{
+					$path = preg_match('/^https?:\/\//i', $css) ? $css : $baseUrl . '/' . $css;
+					$cssFiles[$path] = '';
+				}
 			}
 		}
 		// merge in place
