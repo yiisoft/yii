yiisoft/yii

commit b6811adaced3b29351b3ce83d5abd17f214a794e

Fixes #2921: Fixed CStatePersister read/write concurrency issue causing state data corruption

framework/base/CStatePersister.php
@@ -81,20 +81,45 @@ class CStatePersister extends CApplicationComponent implements IStatePersister
 			$cacheKey='Yii.CStatePersister.'.$stateFile;
 			if(($value=$cache->get($cacheKey))!==false)
 				return unserialize($value);
-			elseif(($content=@file_get_contents($stateFile))!==false)
-			{
-				$cache->set($cacheKey,$content,0,new CFileCacheDependency($stateFile));
-				return unserialize($content);
-			}
 			else
-				return null;
+			{
+				if(($content=$this->getContent($stateFile))!==false)
+				{
+					$unserialized_content=unserialize($content);
+					// If it can't be unserialized, don't cache it:
+					if ($unserialized_content!==false || $content=="") 
+						$cache->set($cacheKey,$content,0,new CFileCacheDependency($stateFile));
+					return $unserialized_content;
+				}
+				else
+					return null;
+			}
 		}
-		elseif(($content=@file_get_contents($stateFile))!==false)
+		elseif(($content=$this->getContent($stateFile))!==false)
 			return unserialize($content);
 		else
 			return null;
 	}
-
+	
+	/**
+	 * Loads content from file using a shared lock to avoid data corruption when reading
+	 * the file while it is being written by save()
+	 *
+	 * @return string file contents
+	 */
+	protected function getContent($filename)
+	{
+		$file=fopen($filename,"r");
+		if($file && flock($file,LOCK_SH))
+		{
+			$contents=@file_get_contents($filename);
+			flock($file,LOCK_UN);
+			fclose($file);
+			return $contents;
+		}
+		return false;
+	}
+	
 	/**
 	 * Saves application state in persistent storage.
 	 * @param mixed $state state data (must be serializable).

commit 5256a1b75526a3dff8fd2d81c39d492a31237f36

Fixed issue introduced by b6811adaced3b29351b3ce83d5abd17f214a794e

framework/base/CStatePersister.php
@@ -109,7 +109,7 @@ class CStatePersister extends CApplicationComponent implements IStatePersister
 	 */
 	protected function getContent($filename)
 	{
-		$file=fopen($filename,"r");
+		$file=@fopen($filename,"r");
 		if($file && flock($file,LOCK_SH))
 		{
 			$contents=@file_get_contents($filename);