97f01cdd94f9e6327ee3ec28f9ef43839391cec8
Matt Dziuban <mrdziuban@gmail.com>
2015-01-27 14:34:08 -0500

    Only do unique joins.

--- framework/db/ar/CActiveFinder.php
+++ framework/db/ar/CActiveFinder.php
@@ -1262,7 +1262,7 @@ class CJoinQuery
 	public function createCommand($builder)
 	{
 		$sql=($this->distinct ? 'SELECT DISTINCT ':'SELECT ') . implode(', ',$this->selects);
-		$sql.=' FROM ' . implode(' ',$this->joins);
+		$sql.=' FROM ' . implode(' ',array_unique($this->joins));
 
 		$conditions=array();
 		foreach($this->conditions as $condition)
