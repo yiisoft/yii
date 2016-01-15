4ab6ca4e3afb2b754940dca092b26ab5a4c67f67
Guillaume Smaha <guillaume.smaha@gmail.com>
2016-01-15 13:36:05 +0100

    Fix #3476: Database sessions with Postgres did not work properly

--- framework/db/CDbConnection.php
+++ framework/db/CDbConnection.php
@@ -583,6 +583,25 @@ class CDbConnection extends CApplicationComponent
 	}
 
 	/**
+	 * Quotes a string value for use in a query.
+	 * @param string $str string to be quoted
+	 * @param integer $quoteParam Parameter for PDO::quote function.
+	 * @return string the properly quoted string
+	 * @see http://www.php.net/manual/en/function.PDO-quote.php
+	 */
+	public function quoteValueExtended($str, $quoteParam)
+	{
+		if(is_int($str) || is_float($str))
+			return $str;
+
+		$this->setActive(true);
+		if(($value=$this->_pdo->quote($str, $quoteParam))!==false)
+			return $value;
+		else  // the driver doesn't support quote (e.g. oci)
+			return "'" . addcslashes(str_replace("'", "''", $str), "\000\n\r\\\032") . "'";
+	}
+
+	/**
 	 * Quotes a table name for use in a query.
 	 * If the table name contains schema prefix, the prefix will also be properly quoted.
 	 * @param string $name table name
