f2054c053691dcf6ecede07764ae3dc511862f25
Alexander Makarov <sam@rmcreative.ru>
2016-04-19 19:29:31 +0300

    More PHP 7 compatibility fixes #4033

--- framework/gii/components/Pear/Text/Diff3.php
+++ framework/gii/components/Pear/Text/Diff3.php
@@ -155,7 +155,7 @@ class Text_Diff3 extends Text_Diff {
  */
 class Text_Diff3_Op {
 
-    function Text_Diff3_Op($orig = false, $final1 = false, $final2 = false)
+    function __construct($orig = false, $final1 = false, $final2 = false)
     {
         $this->orig = $orig ? $orig : array();
         $this->final1 = $final1 ? $final1 : array();
