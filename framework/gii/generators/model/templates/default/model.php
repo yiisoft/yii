54f8fb42a0fa9689fbb43da85c2c01b563a15f08
kdchaires <karina.chaires@cimat.mx>
2016-10-17 00:13:10 -0500

    add labels languages by default
    add labels languages by default

--- framework/gii/generators/model/templates/default/model.php
+++ framework/gii/generators/model/templates/default/model.php
@@ -98,7 +98,7 @@ class <?php echo $modelClass; ?> extends <?php echo $this->baseClass."\n"; ?>
 	{
 		return array(
 <?php foreach($labels as $name=>$label): ?>
-			<?php echo "'".$name."' => '".str_replace("'","\'",$label)."',\n"; ?>
+			<?php echo "'".$name."' => Yii::t('app','".str_replace("'","\'",$label)."'),\n"; ?>
 <?php endforeach; ?>
 		);
 	}
