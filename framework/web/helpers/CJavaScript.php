cc31637ee81b60409b4ddfee266e7d1ac333c4dd
Alexander Makarov <sam@rmcreative.ru>
2017-04-18 14:22:10 +0300

    Fixes #4043: Fixed `CJavaScript::quote()` to properly escape Unicode characters

--- framework/web/helpers/CJavaScript.php
+++ framework/web/helpers/CJavaScript.php
@@ -27,10 +27,12 @@ class CJavaScript
 	 */
 	public static function quote($js,$forUrl=false)
 	{
-		if($forUrl)
-			return strtr($js,array('%'=>'%25',"\t"=>'\t',"\n"=>'\n',"\r"=>'\r','"'=>'\"','\''=>'\\\'','\\'=>'\\\\','</'=>'<\/'));
-		else
-			return strtr($js,array("\t"=>'\t',"\n"=>'\n',"\r"=>'\r','"'=>'\"','\''=>'\\\'','\\'=>'\\\\','</'=>'<\/'));
+        Yii::import('system.vendors.zend-escaper.Escaper');
+        $escaper=new Escaper(Yii::app()->charset);
+        if($forUrl)
+            return $escaper->escapeUrl($js);
+        else
+            return $escaper->escapeJs($js);
 	}
 
 	/**
--- tests/framework/web/helpers/CJavaScriptTest.php
+++ tests/framework/web/helpers/CJavaScriptTest.php
@@ -13,12 +13,28 @@ class CJavaScriptTest extends CTestCase
 	public function testLegacyEncodeSafe()
 	{
 		$expression=CJavaScript::encode("js:function() { /* callback */ }",true);
-		$this->assertEquals("'js:function() { /* callback */ }'",$expression);
+		$this->assertEquals("'js\\x3Afunction\\x28\\x29\\x20\\x7B\\x20\\x2F\\x2A\\x20callback\\x20\\x2A\\x2F\\x20\\x7D'",$expression);
 	}
 
-	public function testEncode()
-	{
-		$expression=CJavaScript::encode(new CJavaScriptExpression("function() { /* callback */ }"));
-		$this->assertEquals("function() { /* callback */ }",$expression);
-	}
+    public function testEncode()
+    {
+        $expression=CJavaScript::encode(new CJavaScriptExpression("function() { /* callback */ }"));
+        $this->assertEquals("function() { /* callback */ }",$expression);
+    }
+
+    public function testQuote()
+    {
+        $input='ro cks!
+            test';
+        $output=CJavaScript::quote($input);
+        $this->assertEquals('ro\u2028cks\x21\x0D\x0A\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20test',$output);
+    }
+
+    public function testQuoteForUrl()
+    {
+        $input='ro cks!
+            test';
+        $output=CJavaScript::quote($input,true);
+        $this->assertEquals('ro%E2%80%A8cks%21%0D%0A%20%20%20%20%20%20%20%20%20%20%20%20test',$output);
+    }
 }
