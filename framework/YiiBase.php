20d91d13b74ed2972892895fbac6c13726483e63
Carsten Brandt <mail@cebe.cc>
2017-06-08 09:22:03 +0200

    prepare for next release

--- framework/YiiBase.php
+++ framework/YiiBase.php
@@ -80,7 +80,7 @@ class YiiBase
 	 */
 	public static function getVersion()
 	{
-		return '1.1.19';
+		return '1.1.20-dev';
 	}
 
 	/**
--- framework/yiilite.php
+++ framework/yiilite.php
@@ -40,7 +40,7 @@ class YiiBase
 	private static $_logger;
 	public static function getVersion()
 	{
-		return '1.1.19';
+		return '1.1.20-dev';
 	}
 	public static function createWebApplication($config=null)
 	{
