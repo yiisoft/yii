53d9900dae593e68eb15d5647e58dcf04f5575b1
Alexander Makarov <sam@rmcreative.ru>
2018-06-03 16:32:30 +0300

    Fixed naming

--- framework/db/schema/mssql/CMssqlSqlsrvPdoAdapter.php
+++ framework/db/schema/mssql/CMssqlSqlsrvPdoAdapter.php
@@ -32,8 +32,8 @@ class CMssqlSqlsrvPdoAdapter extends PDO
 	 */
 	public function lastInsertId($sequence=null)
 	{
-		$tmp = explode('.', phpversion('pdo_sqlsrv'));
-		$sqlsrvVer = phpversion('pdo_sqlsrv') ? intval(array_shift($tmp)) : 0;
+		$parts = explode('.', phpversion('pdo_sqlsrv'));
+		$sqlsrvVer = phpversion('pdo_sqlsrv') ? intval(array_shift($parts)) : 0;
 
 		if(!$sequence || $sqlsrvVer >= 5)
 			return parent::lastInsertId();
