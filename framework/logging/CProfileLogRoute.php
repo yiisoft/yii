fd725843e451318b4221793f316d6228e3824194
Anatoly Pashin <anatoly.pashin@gmail.com>
2018-11-29 12:47:12 +1000

    Remove create_function

--- framework/logging/CProfileLogRoute.php
+++ framework/logging/CProfileLogRoute.php
@@ -179,10 +179,11 @@ class CProfileLogRoute extends CWebLogRoute
 		}
 
 		$entries=array_values($results);
-		$func=create_function('$a,$b','return $a[4]<$b[4]?1:0;');
-		usort($entries,$func);
+        usort($entries, function ($a, $b) {
+            return $a[4] < $b[4] ? 1 : 0;
+        });
 
-		$this->render('profile-summary',$entries);
+        $this->render('profile-summary', $entries);
 	}
 
 	/**
