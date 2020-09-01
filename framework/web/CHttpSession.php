yiisoft/yii

commit 566cae7fd94cffb8a20771949c8a97a27bd237f8

Fix #4308: PHP 7.3 compatibility: Add `samesite` as a session cookie option

framework/web/CHttpSession.php
@@ -241,7 +241,7 @@ class CHttpSession extends CApplicationComponent implements IteratorAggregate,Ar
 	 * The effect of this method only lasts for the duration of the script.
 	 * Call this method before the session starts.
 	 * @param array $value cookie parameters, valid keys include: lifetime, path,
-	 * domain, secure, httponly. Note that httponly is all lowercase.
+	 * domain, secure, httponly, samesite. Note that httponly and samesite is all lowercase.
 	 * @see http://us2.php.net/manual/en/function.session-set-cookie-params.php
 	 */
 	public function setCookieParams($value)
@@ -250,7 +250,17 @@ class CHttpSession extends CApplicationComponent implements IteratorAggregate,Ar
 		extract($data);
 		extract($value);
 		$this->freeze();
-		if(isset($httponly))
+		if(isset($httponly) && isset($sameSite))
+		if(version_compare(PHP_VERSION,'7.3.0','>='))
+			session_set_cookie_params(array('lifetime'=>$lifetime,'path'=>$path,'domain'=>$domain,'secure'=>$secure,'httponly'=>$httponly,'samesite'=>$samesite));
+		else
+		{
+			// Work around for setting sameSite cookie prior PHP 7.3
+			// https://stackoverflow.com/questions/39750906/php-setcookie-samesite-strict/46971326#46971326
+			$path .= '; samesite=' . $sameSite;
+			session_set_cookie_params($lifetime,$path,$domain,$secure,$httponly);
+		}
+		else if(isset($httponly))
 			session_set_cookie_params($lifetime,$path,$domain,$secure,$httponly);
 		else
 			session_set_cookie_params($lifetime,$path,$domain,$secure);

commit e7995a4ac992035422db027eb7e89931a304ca22

Allow setting samesite correctly when calling setCookieParams()
Fixes bug introduced in #4308

framework/web/CHttpSession.php
@@ -250,14 +250,14 @@ class CHttpSession extends CApplicationComponent implements IteratorAggregate,Ar
 		extract($data);
 		extract($value);
 		$this->freeze();
-		if(isset($httponly) && isset($sameSite))
+		if(isset($httponly) && isset($samesite))
 		if(version_compare(PHP_VERSION,'7.3.0','>='))
 			session_set_cookie_params(array('lifetime'=>$lifetime,'path'=>$path,'domain'=>$domain,'secure'=>$secure,'httponly'=>$httponly,'samesite'=>$samesite));
 		else
 		{
 			// Work around for setting sameSite cookie prior PHP 7.3
 			// https://stackoverflow.com/questions/39750906/php-setcookie-samesite-strict/46971326#46971326
-			$path .= '; samesite=' . $sameSite;
+			$path .= '; samesite=' . $samesite;
 			session_set_cookie_params($lifetime,$path,$domain,$secure,$httponly);
 		}
 		else if(isset($httponly))