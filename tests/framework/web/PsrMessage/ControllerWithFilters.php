c64b2006a5712410debb47c817fc99a014f0ac26
Anatoly Pashin <anatoly.pashin@gmail.com>
2020-12-30 15:27:52 +1000

    Fix

--- tests/framework/web/PsrMessage/ControllerWithFilters.php
+++ tests/framework/web/PsrMessage/ControllerWithFilters.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace yii1\tests\framework\web\PsrMessage;
 
 use CController;
+use Laminas\Diactoros\Response\EmptyResponse;
 use Psr\Http\Message\ResponseInterface;
-use Zend\Diactoros\Response\EmptyResponse;
 
 class ControllerWithFilters extends CController
 {
