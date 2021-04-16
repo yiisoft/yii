259ef5e9ed6f38199420bbd90e210c55812948bf
Marco van 't Wout <marco@tremani.nl>
2021-04-16 11:16:46 +0200

    Add the two missing backtrace entries in the error handlers message to Yii::log($msg, CLogger::LEVEL_ERROR, 'php');

--- framework/base/CApplication.php
+++ framework/base/CApplication.php
@@ -802,9 +802,7 @@ abstract class CApplication extends CModule
 
 			$log="$message ($file:$line)\nStack trace:\n";
 			$trace=debug_backtrace();
-			// skip the first 3 stacks as they do not tell the error position
-			if(count($trace)>3)
-				$trace=array_slice($trace,3);
+			array_shift($trace);
 			foreach($trace as $i=>$t)
 			{
 				if(!isset($t['file']))
