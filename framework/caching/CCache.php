6db1f73ec16b205268d89bb77554e2d972c08c47
Ken Guest <kguest@php.net>
2022-08-31 16:50:36 +0100

    unserialize can not be called with null argument

--- framework/caching/CCache.php
+++ framework/caching/CCache.php
@@ -105,7 +105,9 @@ abstract class CCache extends CApplicationComponent implements ICache, ArrayAcce
 		if($value===false || $this->serializer===false)
 			return $value;
 		if($this->serializer===null)
-			$value=unserialize($value);
+            $value=false;
+            if($value!==null)
+			    $value=unserialize($value);
 		else
 			$value=call_user_func($this->serializer[1], $value);
 		if(is_array($value) && (!$value[1] instanceof ICacheDependency || !$value[1]->getHasChanged()))
