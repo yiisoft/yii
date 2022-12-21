b46da1a49e0b52fa3d2d998f24d431b456a7f56a
Tommy <tommy@tremani.nl>
2022-12-21 13:53:48 +0100

    FIX: cast $selection to string as it might be null and cause deprecation erros

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 if (PHP_MAJOR_VERSION>=7 && PHP_MINOR_VERSION>=1) {
 	// skip deprecation errors in PHP 7.1 and above
-	error_reporting(E_ALL & ~E_DEPRECATED);
+	error_reporting(E_ALL);
 }
 
 defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER',false);
