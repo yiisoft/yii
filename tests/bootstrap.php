7b522565ac7b3ed6c07be4b085de8d18a54a21d8
worldtravelerr <sfarcacha@outlook.com>
2023-08-12 18:41:07 -0500

    Update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,23 +3,23 @@
 defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER', false);
 defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', false);
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-$_SERVER['SCRIPT_NAME']     = '/' . basename(__FILE__);
+$_SERVER['SCRIPT_NAME'] = '/' . basename(__FILE__);
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
-if (version_compare(PHP_VERSION, '5.3', '>=')) {
-    require_once(dirname(__FILE__) . '/compatibility.php');
+if (PHP_VERSION_ID >= 50300) {
+    require_once(__DIR__ . '/compatibility.php');
 }
 
-require_once(dirname(__FILE__) . '/../framework/yii.php');
-require_once(dirname(__FILE__) . '/TestApplication.php');
+require_once(__DIR__ . '/../framework/yii.php');
+require_once(__DIR__ . '/TestApplication.php');
 // Support PHPUnit <=3.7 and >=3.8
-if (@include_once('PHPUnit/Framework/TestCase.php') === false) { // <= 3.7
+if (@include ('PHPUnit/Framework/TestCase.php') === false) { // <= 3.7
     require_once('src/Framework/TestCase.php');
 } // >= 3.8
 
 // make sure non existing PHPUnit classes do not break with Yii autoloader
 Yii::$enableIncludePath = false;
-Yii::setPathOfAlias('tests', dirname(__FILE__));
+Yii::setPathOfAlias('tests', __DIR__);
 Yii::import('tests.*');
 
 class CTestCase extends PHPUnit_Framework_TestCase
