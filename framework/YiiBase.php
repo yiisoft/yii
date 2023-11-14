cff207ca26716a88ebdefab2194e8ba6a0990c46
Marco van 't Wout <marco@tremani.nl>
2023-11-14 10:29:46 +0100

    Prepare for next release

--- framework/YiiBase.php
+++ framework/YiiBase.php
@@ -87,7 +87,7 @@ class YiiBase
 	 */
 	public static function getVersion()
 	{
-		return '1.1.29';
+		return '1.1.30-dev';
 	}
 
 	/**
--- framework/yiilite.php
+++ framework/yiilite.php
@@ -41,7 +41,7 @@ class YiiBase
 	private static $_logger;
 	public static function getVersion()
 	{
-		return '1.1.29';
+		return '1.1.30-dev';
 	}
 	public static function createWebApplication($config=null)
 	{
