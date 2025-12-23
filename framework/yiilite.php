fc421693878d34e37b15dfe0624a70275d8f396f
Marco van 't Wout <marco@tremani.nl>
2025-12-23 10:57:28 +0100

    Prepare for next release

--- framework/yiilite.php
+++ framework/yiilite.php
@@ -41,7 +41,7 @@ class YiiBase
 	private static $_logger;
 	public static function getVersion()
 	{
-		return '1.1.32';
+		return '1.1.33-dev';
 	}
 	public static function createWebApplication($config=null)
 	{
